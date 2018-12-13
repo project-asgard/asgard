bace91d16c4cde9ac6f1b6166da303a61ad91d66
Benjamin T. McDaniel <3bm@fusiont5.ornl.gov>
2018-12-13 11:52:13 -0500

    Provide description of element table and its interface

--- src/element_table.cpp
+++ src/element_table.cpp
@@ -5,9 +5,26 @@
 #include "tensors.hpp"
 #include <vector>
 
+
+//TODO this constructor will invoke the 
+//static permutation/cell builder helpers to construct the table
+element_table::element_table(int const dim, int const level, bool const full_grid) : size_{0} {}
+
+//TODO forward lookup
+int element_table::get_index(std::vector<int> const coords) const {
+    return 0;
+}
+
+//TODO reverse lookup
+std::vector<int> element_table::get_coords(int const index) const {
+    return std::vector<int>(0);
+}
+
+
+/*
 //
 // Permutations functions to build level combinations
-//
+// in prgs
 
 static int permutations_eq_count(int dims, int n)
 {
@@ -38,9 +55,9 @@ static int permutations_leq_count(int dims, int n)
 
 //
 // Given dims and n, produce dims-tuples whose sum is equal to n
-//
+// TODO in prgs
 
-static fk::matrix permutations_eq(int dims, int n)
+static fk::matrix permutations_eq(int const dims, int const n)
 {
 
 int const num_tuples = permutations_eq_count(dims, n);
@@ -73,39 +90,42 @@ return result;
 //
 // Given dims and n, produce dims-tuples whose sum is less than or equal to n
 //
-static fk::matrix permutations_leq(int dims, int n)
+
+static fk::matrix permutations_leq(int const dims, int const n, bool const order_by_n)
 {
 
-  int const tuples_count = permutations_leq_count(dims, n);
-  fk::matrix result(tuples_count, dims);
-  
+  int const rows_result = permutations_leq_count(dims, n);
+  fk::matrix result(rows_result, dims);
+ 
+  if(order_by_n) {
+    int row_pos = 0;
+    for(auto i = 0; i<=n; ++i) {
+      int const num_rows = permutations_eq_count(dims, i);
+      fk::matrix partial = permutations_eq(dims, i);
+      result.set_submatrix(count, 0, partial);
+      row_pos += num_rows; 
+    }
+    return result;
+  }
+
   if (dims == 1) {
       std::vector<int> entries(n+1);
       std::iota(begin(entries), end(entries), 0);
-      for (auto i = 0u; i < entries.size(); ++i) {
-          result(i,1) = entries[i];
-      }
-   return result;
+      result.update_col(0, entries);
+      //for (auto i = 0; i < entries.size(); ++i) {
+      //    result[i] =i std::array<int,dims>{entries[i]};
+      //}
+      return result;
   }
  
-  int counter = 0;
+  int row_pos = 0;
   for(auto i = 0; i <= n; ++i) {
-    int const size = permutations_leq_count(dims-1, n-1);
-    fk::matrix partial  
+      int const num_rows = permutations_leq_count(dims-1, n-i);
+      fk::matrix partial = permutations_leq(dims, i);
+      result.set_submatrix(count, 0, partial);
+      //std::vector<double> partial_last_col(
+      row_pos += num_rows; 
   }
 }
-/*
-  ip = 1;
-    for
-      i = 0 : n, isize = perm_leq_count(idim - 1, n - i);
-    result(ip
-           : (ip + isize - 1), 1
-           : (idim - 1))                = perm_leq(idim - 1, n - i, order_by_n);
-    result(ip : (ip + isize - 1), idim) = i;
 
-    ip = ip + isize;
-    end;
-
-    return
-    */
-  
+*/
--- src/element_table.hpp
+++ src/element_table.hpp
@@ -3,3 +3,39 @@
 #include <vector>
 
 
+// --------------------------------------------------------------------------
+// element table
+// this object's purpose is:
+// - to provide a mapping from elements' coordinates to an assigned positive 
+// integer index
+// - to provide the reverse mapping from index to coordinates.
+// 
+// about coordinates
+// - coordinates are composed of dimension-many (lev, cell) tuples.
+// (technically, an element's coordinates will also have a degree level,
+// but we omit this as we assume uniform degree).
+// - in our table, coordinates are stored with all level components grouped,
+// followed by cell components.
+// ex: (lev_1, lev_2, ..., lev_dim, cell_1, cell_2, ..., cell_dim).
+//
+// re: full and sparse grid
+// - to build a full grid, all potentional level combinations are included in
+// the table; that is, all dimension-length permutations of integers less than
+// or equal to the number of levels selected for the simulation are valid level
+// components. 
+// - to build a sparse grid, we apply some rule to omit some of these permutations.
+// currently, we cull level combinations whose sum is greater than the number of
+// levels selected for the simulation.   
+// --------------------------------------------------------------------------
+
+class element_table 
+{
+public:
+  element_table(int const dim, int const level, bool const full_grid=false);
+  int get_index(std::vector<int> const coords) const;
+  std::vector<int> get_coords(int const index) const;
+  int size() const { return size_; } 
+private:
+  int size_;
+};
+
