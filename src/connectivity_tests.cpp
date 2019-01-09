0dfc91cc8a492a81d54399ff0754b92ef4c80118
Benjamin T. McDaniel <3bm@fusiont5.ornl.gov>
2019-01-09 10:26:43 -0500

    Finish connectivity tests

--- src/connectivity_tests.cpp
+++ src/connectivity_tests.cpp
@@ -3,6 +3,7 @@
 #include "element_table.hpp"
 #include "matlab_utilities.hpp"
 #include "tests_general.hpp"
+#include <numeric>
 #include <string>
 #include <vector>
 
@@ -47,7 +48,7 @@ TEST_CASE("n-dimensional connectivity", "[connectivity]")
 {
   SECTION("2 dimensions, level 2, sparse grid")
   {
-    list_set gold = list_set(16, {0, 1, 2, 3, 4, 5, 6, 7});
+    list_set gold = list_set(8, {0, 1, 2, 3, 4, 5, 6, 7});
 
     int const levels = 2;
     int const dims   = 2;
@@ -57,22 +58,81 @@ TEST_CASE("n-dimensional connectivity", "[connectivity]")
     list_set connectivity = make_connectivity(t, dims, levels, levels);
     REQUIRE(connectivity == gold);
   }
-  SECTION("2 dimensions, level 3, sparse grid")
+  SECTION("2 dimensions, level 2, full grid")
   {
-     
-    // clang-format off
-    list_set gold = lis	  
+    list_set gold =
+        list_set(16, {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15});
 
-    // clang-format off
-    int const levels = 3;
+    int const levels = 2;
     int const dims   = 2;
     Options o        = make_options({"-l", std::to_string(levels), "-f"});
     element_table t(o, dims);
+
+    list_set connectivity = make_connectivity(t, dims, levels * dims, levels);
+    REQUIRE(connectivity == gold);
   }
-  SECTION("3 dimensions, level 2, sparse grid")
+  SECTION("3 dimensions, level 3, sparse grid")
   {
-    int const levels = 2;
-    int const dims   = 2;
+    int const num_elements = 38;
+    std::vector<int> fully_connected(num_elements);
+    std::iota(fully_connected.begin(), fully_connected.end(), 0);
+    list_set gold;
+    for (auto i = 0; i < num_elements; ++i)
+    {
+      std::vector<int> element_i = fully_connected;
+      auto start                 = element_i.begin();
+      if (i == 4)
+      {
+        element_i.erase(start + 6);
+      }
+      else if (i == 5)
+      {
+        element_i.erase(start + 7);
+      }
+      else if (i == 6)
+      {
+        element_i.erase(start + 4);
+      }
+      else if (i == 7)
+      {
+        element_i.erase(start + 5);
+      }
+      else if (i == 16)
+      {
+        element_i.erase(start + 18);
+      }
+      else if (i == 17)
+      {
+        element_i.erase(start + 19);
+      }
+      else if (i == 18)
+      {
+        element_i.erase(start + 16);
+      }
+      else if (i == 19)
+      {
+        element_i.erase(start + 17);
+      }
+      else if (i == 34)
+      {
+        element_i.erase(start + 36);
+      }
+      else if (i == 35)
+      {
+        element_i.erase(start + 37);
+      }
+      else if (i == 36)
+      {
+        element_i.erase(start + 34);
+      }
+      else if (i == 37)
+      {
+        element_i.erase(start + 35);
+      }
+      gold.push_back(element_i);
+    }
+    int const levels = 3;
+    int const dims   = 3;
     Options o        = make_options({"-l", std::to_string(levels)});
     element_table t(o, dims);
   }
