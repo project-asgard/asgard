7ee7835cc2ef1ce2335d26f7eeb3eb3e59584ba7
Graham Lopez <lopezmg@ornl.gov>
2019-01-23 14:53:37 -0500

    clean up comments

--- src/connectivity.hpp
+++ src/connectivity.hpp
@@ -10,9 +10,10 @@
 // elements in the element_table
 // -----------------------------------------------------------------------------
 
-// FIXME need to determine which of these need to be
 int get_1d_index(int const level, int const cell);
+
 fk::matrix<int> make_1d_connectivity(int const num_levels);
+
 using list_set = std::vector<fk::vector<int>>;
 list_set make_connectivity(element_table table, int const num_dims,
                            int const max_level_sum, int const max_level_val,
