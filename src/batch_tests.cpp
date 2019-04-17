92ef1be785f3bf96dc4043ca58d6bf0fbff2fd2a
Benjamin T. McDaniel <3bm@fusiont5.ornl.gov>
2019-04-17 16:14:36 -0400

    Add transposed a batch gemm test

--- src/batch_tests.cpp
+++ src/batch_tests.cpp
@@ -350,4 +350,105 @@ TEMPLATE_TEST_CASE("batched gemm", "[batch]", float, double)
     // compare
     REQUIRE(c == gold);
   }
+
+  auto get_trans =
+      [](fk::matrix<TestType, mem_type::view> orig) -> fk::matrix<TestType> {
+    fk::matrix<TestType> builder(orig);
+    return builder.transpose();
+  };
+
+  SECTION("batched gemm: trans a, no trans b, alpha = 1.0, beta = 0.0")
+  {
+    // make 3x2 (pre-trans) "a" views
+    int const a_start_row = 1;
+    int const a_stop_row  = 3;
+    int const a_nrows     = a_stop_row - a_start_row + 1;
+    int const a_start_col = 0;
+    int const a_stop_col  = 1;
+    int const a_ncols     = a_stop_col - a_start_col + 1;
+    int const a_stride    = a1.nrows();
+
+    fk::matrix<TestType, mem_type::view> const a1_v(a1, a_start_row, a_stop_row,
+                                                    a_start_col, a_stop_col);
+    fk::matrix<TestType, mem_type::view> const a2_v(a2, a_start_row, a_stop_row,
+                                                    a_start_col, a_stop_col);
+    fk::matrix<TestType, mem_type::view> const a3_v(a3, a_start_row, a_stop_row,
+                                                    a_start_col, a_stop_col);
+
+    // make the transposed versions for forming golden value
+    fk::matrix<TestType> const a1_t = get_trans(a1_v);
+    fk::matrix<TestType> const a2_t = get_trans(a2_v);
+    fk::matrix<TestType> const a3_t = get_trans(a3_v);
+
+    batch_list<TestType> const a_batch = [&] {
+      batch_list<TestType> builder(num_batch, a_nrows, a_ncols, a_stride);
+
+      builder.insert(a1_v, 0);
+      builder.insert(a2_v, 1);
+      builder.insert(a3_v, 2);
+
+      return builder;
+    }();
+
+    // make 3x2 "b" views
+    int const b_start_row = 1;
+    int const b_stop_row  = 3;
+    int const b_nrows     = b_stop_row - b_start_row + 1;
+    int const b_start_col = 1;
+    int const b_stop_col  = 2;
+    int const b_ncols     = b_stop_col - b_start_col + 1;
+    int const b_stride    = b1.nrows();
+
+    fk::matrix<TestType, mem_type::view> const b1_v(b1, b_start_row, b_stop_row,
+                                                    b_start_col, b_stop_col);
+    fk::matrix<TestType, mem_type::view> const b2_v(b2, b_start_row, b_stop_row,
+                                                    b_start_col, b_stop_col);
+    fk::matrix<TestType, mem_type::view> const b3_v(b3, b_start_row, b_stop_row,
+                                                    b_start_col, b_stop_col);
+
+    batch_list<TestType> const b_batch = [&] {
+      batch_list<TestType> builder(num_batch, b_nrows, b_ncols, b_stride);
+
+      builder.insert(b1_v, 0);
+      builder.insert(b2_v, 1);
+      builder.insert(b3_v, 2);
+
+      return builder;
+    }();
+
+    // make 3x2 "c" views
+    fk::matrix<TestType> c(6, 2);
+    fk::matrix<TestType, mem_type::view> c1_v(c, 0, 1, 0, 1);
+    fk::matrix<TestType, mem_type::view> c2_v(c, 2, 3, 0, 1);
+    fk::matrix<TestType, mem_type::view> c3_v(c, 4, 5, 0, 1);
+
+    batch_list<TestType> const c_batch = [&] {
+      batch_list<TestType> builder(num_batch, a_ncols, b_ncols, c.nrows());
+      builder.insert(c1_v, 0);
+      builder.insert(c2_v, 1);
+      builder.insert(c3_v, 2);
+      return builder;
+    }();
+
+    // do the math to create gold matrix
+    fk::matrix<TestType> gold(6, 2);
+    fk::matrix<TestType, mem_type::view> gold1_v(gold, 0, 1, 0, 1);
+    fk::matrix<TestType, mem_type::view> gold2_v(gold, 2, 3, 0, 1);
+    fk::matrix<TestType, mem_type::view> gold3_v(gold, 4, 5, 0, 1);
+
+    gold1_v = a1_t * b1_v;
+    gold2_v = a2_t * b2_v;
+    gold3_v = a3_t * b3_v;
+
+    // call batched gemm
+    TestType alpha = 1.0;
+    TestType beta  = 0.0;
+    bool trans_a   = true;
+    bool trans_b   = false;
+
+    batched_gemm(a_batch, b_batch, c_batch, alpha, beta, trans_a, trans_b);
+
+    // compare
+    REQUIRE(c == gold);
+  }
 }
