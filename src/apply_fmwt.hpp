20b26a9cc36fb9a7dbbe2a808041942f7837e05e
Timothy Younkin <tyounkin@Timothys-MacBook-Pro.local>
2019-04-24 14:34:11 -0400

    working version of apply fmwt

--- src/apply_fmwt.hpp
+++ src/apply_fmwt.hpp
@@ -10,10 +10,23 @@
 #include <vector>
 
 template<typename P>
-fk::matrix<P> apply_fmwt(fk::matrix<P> matrix1,fk::matrix<P> matrix2);
+fk::matrix<P> reshape(fk::matrix<P> mat, int const dim1, int const dim2);
 
 extern template fk::matrix<double>
-apply_fmwt(fk::matrix<double> matrix1,fk::matrix<double> matrix2);
+reshape(fk::matrix<double> mat, int const dim1, int const dim2);
 extern template fk::matrix<float>
-apply_fmwt(fk::matrix<float> matrix1,fk::matrix<float> matrix2);
+reshape(fk::matrix<float> mat, int const dim1, int const dim2);
 
+template<typename P>
+fk::matrix<P> apply_fmwt(fk::matrix<P> matrix1, fk::matrix<P> matrix2,
+                         int const kdeg, int const lev, int const isLeft,
+                         int const isTrans, int const method);
+
+extern template fk::matrix<double>
+apply_fmwt(fk::matrix<double> matrix1, fk::matrix<double> matrix2,
+           int const kdeg, int const lev, int const isLeft, int const isTrans,
+           int const method);
+extern template fk::matrix<float>
+apply_fmwt(fk::matrix<float> matrix1, fk::matrix<float> matrix2, int const kdeg,
+           int const lev, int const isLeft, int const isTrans,
+           int const method);
