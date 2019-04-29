871e4fb905a0f8e7b43cfbf95b7dac66ab233613
Graham Lopez <lopezmg@ornl.gov>
2019-04-28 22:04:35 -0400

    initial build support for gperftools
    still need to fix a few things:
    - pprof depends on dot (need to set up automagic building as well as
      update docs and/or set up the paths).
    - the cmake stuff to get the linking of libprofiler.so is a bit
      hacky; might want to think about ways to clean it up a bit
    - in general, it would be good to look for the dependencies (e.g.
      graphviz) on the system
    - might consider offering some control over building the dependencies

--- profiling/profiling.cmake
+++ profiling/profiling.cmake
@@ -3,10 +3,15 @@
 ## Profiling support
 ###############################################################################
 
+set (_profile_deps "")
+set (_profile_libs "")
+
 ###############################################################################
 ## GNU gprof
 #
-# FIXME make graphviz build optional
+# FIXME
+#  - make graphviz build optional
+#  - make gprof2dot build optional
 #
 #  if cmake needs to "build graphviz", and it remains from a previous build,
 #  cmake will skip the build and reuse it
@@ -19,9 +24,11 @@ if (ASGARD_PROFILE_GPROF)
       "\n"
       "   gprof enabled. For using gprof:\n"
       "   1) build/link the code with -pg option (will be done for you during 'make')\n"
-      "   2) run the executable to be profiled.\n"
+      "   2) run the executable to be profiled. e.g.\n"
+      "      $ ./asgard -p continuity_6 -l 8 -d 3\n"
       "      this produces a 'gmon.out' in the current directory\n"
-      "   3) gprof asgard gmon.out > [output-file.txt]\n"
+      "   3) analyze the restuls to get the timings and call graph\n"
+      "      $ gprof asgard gmon.out > [output-file.txt]\n"
       "\n"
       "   for more details, see gprof's documentation: https://bit.ly/2UZxdkz\n"
       "\n"
@@ -33,45 +40,45 @@ if (ASGARD_PROFILE_GPROF)
       "\n"
     )
 
-  # build graphviz if needed
-  # TODO detect if it is on the system
-  set (graphviz_PATH ${CMAKE_SOURCE_DIR}/contrib/graphviz)
-  if (NOT EXISTS ${graphviz_PATH}/bin/dot)
-    message (STATUS "graphviz not found. building from source")
-    include (ExternalProject)
-    ExternalProject_Add (graphviz-ext
-      PREFIX contrib/graphviz
-      URL https://graphviz.gitlab.io/pub/graphviz/stable/SOURCES/graphviz.tar.gz
-      DOWNLOAD_NO_PROGRESS 1
-      BUILD_IN_SOURCE 1
-      USES_TERMINAL_CONFIGURE 1
-      CONFIGURE_COMMAND ${CMAKE_CURRENT_BINARY_DIR}/contrib/graphviz/src/graphviz-ext/configure --prefix=${graphviz_PATH}
-      BUILD_COMMAND make
-      INSTALL_COMMAND make install
-    )
-  else ()
-    message (STATUS "using graphviz found at ${graphviz_PATH}")
-  endif ()
+    # build graphviz if needed
+    # TODO detect if it is on the system
+    set (graphviz_PATH ${CMAKE_SOURCE_DIR}/contrib/graphviz)
+    if (NOT EXISTS ${graphviz_PATH}/bin/dot)
+      message (STATUS "graphviz not found. building from source")
+      include (ExternalProject)
+      ExternalProject_Add (graphviz-ext
+        PREFIX contrib/graphviz
+        URL https://graphviz.gitlab.io/pub/graphviz/stable/SOURCES/graphviz.tar.gz
+        DOWNLOAD_NO_PROGRESS 1
+        BUILD_IN_SOURCE 1
+        USES_TERMINAL_CONFIGURE 1
+        CONFIGURE_COMMAND ${CMAKE_CURRENT_BINARY_DIR}/contrib/graphviz/src/graphviz-ext/configure --prefix=${graphviz_PATH}
+        BUILD_COMMAND make -j
+        INSTALL_COMMAND make install
+      )
+    else ()
+      message (STATUS "using graphviz found at ${graphviz_PATH}")
+    endif ()
 
-  # grab gprof2dot (we don't store it in the repo or distribute)
-  set (gprof2dot_PATH ${CMAKE_SOURCE_DIR}/contrib/gprof2dot)
-  if (NOT EXISTS ${gprof2dot_PATH}/bin/gprof2dot.py)
-    message (STATUS "gprof2dot not found. downloading")
-    include (ExternalProject)
-    ExternalProject_Add (gprof2dot-ext
-      PREFIX contrib/gprof2dot
-      URL https://raw.githubusercontent.com/jrfonseca/gprof2dot/master/gprof2dot.py
-      DOWNLOAD_NO_PROGRESS 1
-      DOWNLOAD_NO_EXTRACT 1
-      CONFIGURE_COMMAND ""
-      BUILD_COMMAND ""
-      INSTALL_COMMAND mkdir -p ${gprof2dot_PATH}/bin
-      COMMAND cp ${CMAKE_CURRENT_BINARY_DIR}/contrib/gprof2dot/src/gprof2dot.py ${gprof2dot_PATH}/bin/
-      COMMAND chmod 700 ${gprof2dot_PATH}/bin/gprof2dot.py
-    )
-  else ()
-    message (STATUS "using gprof2dot found at ${gprof2dot_PATH}")
-  endif ()
+    # grab gprof2dot (we don't store it in the repo or distribute)
+    set (gprof2dot_PATH ${CMAKE_SOURCE_DIR}/contrib/gprof2dot)
+    if (NOT EXISTS ${gprof2dot_PATH}/bin/gprof2dot.py)
+      message (STATUS "gprof2dot not found. downloading")
+      include (ExternalProject)
+      ExternalProject_Add (gprof2dot-ext
+        PREFIX contrib/gprof2dot
+        URL https://raw.githubusercontent.com/jrfonseca/gprof2dot/master/gprof2dot.py
+        DOWNLOAD_NO_PROGRESS 1
+        DOWNLOAD_NO_EXTRACT 1
+        CONFIGURE_COMMAND ""
+        BUILD_COMMAND ""
+        INSTALL_COMMAND mkdir -p ${gprof2dot_PATH}/bin
+        COMMAND cp ${CMAKE_CURRENT_BINARY_DIR}/contrib/gprof2dot/src/gprof2dot.py ${gprof2dot_PATH}/bin/
+        COMMAND chmod 700 ${gprof2dot_PATH}/bin/gprof2dot.py
+      )
+    else ()
+      message (STATUS "using gprof2dot found at ${gprof2dot_PATH}")
+    endif ()
 
   else ()
     message (FATAL_ERROR "must use GCC to enable gprof suppport")
@@ -80,6 +87,10 @@ endif ()
 
 ###############################################################################
 ## LLVM XRAY
+#
+# FIXME
+#  - make flamegraph build optional
+#
 ###############################################################################
 
 if (ASGARD_PROFILE_XRAY)
@@ -91,10 +102,31 @@ if (ASGARD_PROFILE_XRAY)
       "   LLVM XRAY enabled. to use:\n"
       "   1) build/link the code with -fxray-instrument option (done for you during 'make')\n"
       "   2) run the executable to be profiled with:\n"
-      "      $ XRAY_OPTIONS=\"patch_premain=true xray_mode=xray-basic verbosity=1\" [./exec] [exec options]\n"
-      "      this produces a uniquely-hashed 'xray-log.[exec].[hash]' file\n"
-      "   3) llvm-xray account xray-log.[exec].[hash] -sort=sum -sortorder=dsc -instr_map [./exec]\n"
+      "      $ XRAY_OPTIONS=\"patch_premain=true xray_mode=xray-basic verbosity=1\" ./asgard -p continuity_6 -l 8 -d 3\n"
+      "      this produces a uniquely-hashed 'xray-log.asgard.[hash]' file\n"
+      "   3) analyze the results with\n"
+      "      $ llvm-xray account xray-log.asgard.[hash] -sort=sum -sortorder=dsc -instr_map ./asgard\n"
     )
+
+    # grab FlameGraph (we don't store it in the repo or distribute)
+    set (flamegraph_PATH ${CMAKE_SOURCE_DIR}/contrib/FlameGraph)
+    if (NOT EXISTS ${flamegraph_PATH}/flamegraph.pl)
+      message (STATUS "flamegraph not found. downloading")
+      include (ExternalProject)
+      ExternalProject_Add (flamegraph-ext
+        SOURCE_DIR ${flamegraph_PATH}
+        GIT_REPOSITORY https://github.com/brendangregg/FlameGraph
+        GIT_PROGRESS 1
+        GIT_SHALLOW 1
+        UPDATE_COMMAND ""
+        CONFIGURE_COMMAND ""
+        BUILD_COMMAND ""
+        INSTALL_COMMAND ""
+      )
+    else ()
+      message (STATUS "using flamegraph.pl found at ${flamegraph_PATH}")
+    endif ()
+
   else ()
     message (FATAL_ERROR "must use clang to enable xray suppport")
   endif ()
@@ -102,10 +134,55 @@ endif ()
 
 ###############################################################################
 ## gperftools (formerly google performance tools)
+#
+# FIXME allow user to point to previously installed libprofiler, etc.
+# FIXME depends on graphviz/dot
+#
 ###############################################################################
 
 if (ASGARD_PROFILE_GPERF)
+  #add_link_options ("-lprofiler")
+    message (
+      "\n"
+      "   gperftools enabled. to use:\n"
+      "   1) link the code with -lprofiler option (done for you during 'make')\n"
+      "   2) run the executable to be profiled with:\n"
+      "      $ CPUPROFILE=some-name.prof ./asgard -p continuity_6 -l 8 -d 3\n"
+      "      this produces a profile file name 'some-name.prof'\n"
+      "   3) analyze the results with\n"
+      "      $ pprof --pdf ./asgard some-name.prof > some-other.pdf\n"
+    )
 
+  # build gperftools if needed
+  set (gperftools_PATH ${CMAKE_SOURCE_DIR}/contrib/gperftools)
+  if (NOT EXISTS ${gperftools_PATH}/bin/pprof)
+    message (STATUS "gperftools not found. building from source")
+    set (_profile_deps gperftools-ext)
+    include (ExternalProject)
+    ExternalProject_Add (gperftools-ext
+      PREFIX contrib/gperftools
+      GIT_REPOSITORY https://github.com/gperftools/gperftools
+      GIT_PROGRESS 1
+      GIT_SHALLOW 1
+      BUILD_IN_SOURCE 1
+      USES_TERMINAL_CONFIGURE 1
+      CONFIGURE_COMMAND ${CMAKE_CURRENT_BINARY_DIR}/contrib/gperftools/src/gperftools-ext/autogen.sh
+      COMMAND ${CMAKE_CURRENT_BINARY_DIR}/contrib/gperftools/src/gperftools-ext/configure --prefix=${gperftools_PATH}
+      BUILD_COMMAND make -j
+      INSTALL_COMMAND make install
+    )
+  else ()
+    message (STATUS "using gperftools found at ${gperftools_PATH}")
+  endif ()
+  set (_profile_libs -Wl,-no-as-needed "${gperftools_PATH}/lib/libprofiler.so")
+endif ()
+
+###############################################################################
+## linux perf
+###############################################################################
+
+if (ASGARD_PROFILE_PERF)
+  message (FATAL_ERROR "linux perf not supported yet :'(. Please submit an issue or PR.")
 endif ()
 
 ###############################################################################
@@ -113,5 +190,5 @@ endif ()
 ###############################################################################
 
 if (ASGARD_PROFILE_VALGRIND)
-
+  message (FATAL_ERROR "valgrind not supported yet :'(. Please submit an issue or PR.")
 endif ()
