dcf64d98962b68cd5b9e6a9c405e806e2083ccbd
Benjamin T. McDaniel <3bm@fusiont5.ornl.gov>
2019-05-17 12:17:20 -0400

    invoke setup routines

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include "batch.hpp"
 #include "coefficients.hpp"
 #include "connectivity.hpp"
 #include "element_table.hpp"
@@ -6,61 +7,60 @@
 #include "tensors.hpp"
 #include "transformations.hpp"
 
+using prec = double;
 int main(int argc, char **argv)
 {
+  // parse user input and generate pde
   options opts(argc, argv);
-  auto pde = make_PDE<double>(opts.get_selected_pde(), opts.get_level(),
-                              opts.get_degree());
+  auto pde = make_PDE<prec>(opts.get_selected_pde(), opts.get_level(),
+                            opts.get_degree());
 
-  element_table table = element_table(opts, pde->num_dims);
+  // create forward/reverse mapping between elements and indices
+  element_table const table = element_table(opts, pde->num_dims);
 
-  // list_set connectivity = make_connectivity(table, pde->num_dims,
-  //                                          opts.get_level(),
-  //                                          opts.get_level());
-
-  // this should probably be wrapped in a function, doing it inline for now
-  std::vector<fk::vector<double>> initial_conditions;
-  for (dimension<double> const dim : pde->get_dimensions())
+  // generate initial condition vector. 
+  prec const initial_scale           = 1.0;
+  std::vector<fk::vector<prec>> initial_conditions;
+  for (dimension<prec> const &dim : pde->get_dimensions())
   {
     initial_conditions.push_back(
-        forward_transform<double>(dim, dim.initial_condition));
+        forward_transform<prec>(dim, dim.initial_condition));
   }
-  double const start = 1.0;
-  // the combine dimensions function will have to be modified for variable
-  // deg/lev among dimensions
-  fk::vector<double> initial_condition = combine_dimensions(
-      pde->get_dimensions()[0], table, initial_conditions, start);
+  fk::vector<prec> initial_condition = combine_dimensions(
+      pde->get_dimensions()[0], table, initial_conditions, initial_scale);
 
-  // same for this, wrapped up in a function
-  // store sources, will scale later for time
-  std::vector<fk::vector<double>> initial_sources;
-  for (source<double> const source : pde->sources)
+  // generate sources.
+  // these will be scaled later for time
+  std::vector<fk::vector<prec>> initial_sources;
+  for (source<prec> const &source : pde->sources)
   {
-    std::vector<fk::vector<double>> initial_sources_dim;
+    std::vector<fk::vector<prec>> initial_sources_dim;
     for (int i = 0; i < pde->num_dims; ++i)
     {
-      initial_sources_dim.push_back(forward_transform<double>(
+      initial_sources_dim.push_back(forward_transform<prec>(
           pde->get_dimensions()[i], source.source_funcs[i]));
     }
 
     initial_sources.push_back(combine_dimensions(
-        pde->get_dimensions()[0], table, initial_sources_dim, start));
+        pde->get_dimensions()[0], table, initial_sources_dim, initial_scale));
   }
 
-  // make the terms. also another function. also, should time be one here, or
-  // zero...
-  double const init_time = 0.0;
-  for (std::vector<term<double>> const terms : pde->get_terms())
+  // generate and store coefficient matrices.
+  prec const initial_time = 0.0;
+  for (int i = 0; i < pde->num_dims; ++i)
   {
-    for (term<double> term : terms)
+    dimension<prec> const dim = pde->get_dimensions()[i];
+    for (int j = 0; j < pde->num_terms; ++j)
     {
-      for (int i = 0; i < pde->num_dims; ++i)
-      {
-        dimension<double> const dim = pde->get_dimensions()[i];
-        term.set_coefficients(dim, generate_coefficients(dim, term, init_time));
-      }
+      term<prec> const partial_term = pde->get_terms()[j][i];
+      fk::matrix<prec> const coeff =
+          generate_coefficients(dim, partial_term, initial_time);
+      pde->set_coefficients(coeff, j, i);
     }
   }
+  
+  // allocate 
+
 
   return 0;
 }
