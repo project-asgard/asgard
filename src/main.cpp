46c2f9bedd111d5210d2192835eca0b5f4beb5fd
Graham Lopez <lopezmg@ornl.gov>
2019-06-03 12:38:48 -0400

    added very basic program options printout

--- src/main.cpp
+++ src/main.cpp
@@ -19,8 +19,6 @@ int main(int argc, char **argv)
 
   options opts(argc, argv);
 
-  std::cout << "--- begin setup ---" << std::endl;
-
   // -- parse user input and generate pde
   std::cout << "generating: pde..." << std::endl;
   auto pde = make_PDE<prec>(opts.get_selected_pde(), opts.get_level(),
@@ -35,12 +33,26 @@ int main(int argc, char **argv)
   // if we ever do go to p-adaptivity (variable degree) we can change it then
   auto const degree = pde->get_dimensions()[0].get_degree();
 
+  std::cout << "ASGarD problem configuration:" << '\n';
+  std::cout << "  selected PDE: " << opts.get_pde_string() << '\n';
+  std::cout << "  level: " << opts.get_level() << '\n';
+  std::cout << "  degree: " << opts.get_degree() << '\n';
+  std::cout << "  N steps: " << opts.get_time_steps() << '\n';
+  std::cout << "  write freq: " << opts.get_write_frequency() << '\n';
+  std::cout << "  vis. freq: " << opts.get_visualization_frequency() << '\n';
+  std::cout << "  implicit: " << opts.using_implicit() << '\n';
+  std::cout << "  full grid: " << opts.using_full_grid() << '\n';
+  std::cout << "  CFL number: " << opts.get_cfl() << '\n';
+  std::cout << "  Poisson solve: " << opts.do_poisson_solve() << '\n';
+
+  std::cout << "--- begin setup ---" << std::endl;
+
   // -- create forward/reverse mapping between elements and indices
-  std::cout << "generating: element table..." << std::endl;
+  std::cout << "  generating: element table..." << std::endl;
   element_table const table = element_table(opts, pde->num_dims);
 
   // -- generate initial condition vector.
-  std::cout << "generating: initial conditions..." << std::endl;
+  std::cout << "  generating: initial conditions..." << std::endl;
   fk::vector<prec> const initial_condition = [&pde, &table, degree]() {
     std::vector<fk::vector<prec>> initial_conditions;
     for (dimension<prec> const &dim : pde->get_dimensions())
@@ -54,7 +66,7 @@ int main(int argc, char **argv)
   // -- generate source vectors.
   // these will be scaled later according to the simulation time applied
   // with their own time-scaling functions
-  std::cout << "generating: source vectors..." << std::endl;
+  std::cout << "  generating: source vectors..." << std::endl;
   std::vector<fk::vector<prec>> const initial_sources = [&pde, &table,
                                                          degree]() {
     std::vector<fk::vector<prec>> initial_sources;
@@ -75,7 +87,7 @@ int main(int argc, char **argv)
   }();
 
   // -- generate analytic solution vector.
-  std::cout << "generating: analytic solution at t=0 ..." << std::endl;
+  std::cout << "  generating: analytic solution at t=0 ..." << std::endl;
   fk::vector<prec> const analytic_solution = [&pde, &table, degree]() {
     std::vector<fk::vector<prec>> analytic_solutions_D;
     for (int d = 0; d < pde->num_dims; d++)
@@ -87,7 +99,7 @@ int main(int argc, char **argv)
   }();
 
   // -- generate and store coefficient matrices.
-  std::cout << "generating: coefficient matrices..." << std::endl;
+  std::cout << "  generating: coefficient matrices..." << std::endl;
   for (int i = 0; i < pde->num_dims; ++i)
   {
     dimension<prec> const dim = pde->get_dimensions()[i];
@@ -114,30 +126,30 @@ int main(int argc, char **argv)
   // input vector x
   int const elem_size =
       std::pow(pde->get_dimensions()[0].get_degree(), pde->num_dims);
-  std::cout << "allocating input vector, size (MB): "
+  std::cout << "  allocating input vector, size (MB): "
             << get_MB(table.size() * elem_size) << std::endl;
   fk::vector<prec> x(initial_condition);
 
   // intermediate output spaces for batched gemm
-  std::cout << "allocating kronmult output space, size (MB): "
+  std::cout << "  allocating kronmult output space, size (MB): "
             << get_MB(x.size() * table.size() * pde->num_terms) << std::endl;
   fk::vector<prec> y(x.size() * table.size() * pde->num_terms);
 
   int const num_workspaces = std::min(pde->num_dims - 1, 2);
-  std::cout << "allocating kronmult working space, size (MB): "
+  std::cout << "  allocating kronmult working space, size (MB): "
             << get_MB(x.size() * table.size() * pde->num_terms * num_workspaces)
             << std::endl;
   fk::vector<prec> work(x.size() * table.size() * pde->num_terms *
                         num_workspaces);
 
   // output vector fval
-  std::cout << "allocating output vector, size (MB): " << get_MB(x.size())
+  std::cout << "  allocating output vector, size (MB): " << get_MB(x.size())
             << std::endl;
   fk::vector<prec> fval(x.size());
 
   // setup reduction vector
   int const items_to_reduce = pde->num_terms * table.size();
-  std::cout << "allocating reduction vector, size (MB): "
+  std::cout << "  allocating reduction vector, size (MB): "
             << get_MB(items_to_reduce) << std::endl;
   fk::vector<prec> const unit_vector = [&] {
     fk::vector<prec> builder(items_to_reduce);
@@ -146,12 +158,12 @@ int main(int argc, char **argv)
   }();
 
   // call to build batches
-  std::cout << "generating: batch lists..." << std::endl;
+  std::cout << "  generating: batch lists..." << std::endl;
   std::vector<batch_operands_set<prec>> const batches =
       build_batches(*pde, table, x, y, work, unit_vector, fval);
 
   // these vectors used for intermediate results in time advance
-  std::cout << "allocating time loop working space, size (MB): "
+  std::cout << "  allocating time loop working space, size (MB): "
             << get_MB(x.size() * 5) << std::endl;
   fk::vector<prec> scaled_source(x.size());
   fk::vector<prec> x_orig(x.size());
