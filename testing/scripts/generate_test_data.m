c940140e1594f00dc89bc1a36a4efa33af65debd
David Green <david.lindsay.green@gmail.com>
2019-06-12 22:29:59 -0400

    Matlab responsibile for generating element_table test data

--- testing/scripts/generate_test_data.m
+++ testing/scripts/generate_test_data.m
@@ -15,47 +15,6 @@ addpath(genpath(pwd));
 warning('on');
 % write output files for each component
 
-
-% element_table testing files
-element_dir = strcat(pwd, "/", "generated-inputs", "/", "element_table", "/");
-mkdir (element_dir);
-
-out_format = strcat(element_dir, "element_table_1_1_SG_%d.dat");
-level = 1;
-dim = 1;
-grid_type = 'SG';
-[fwd1, inv1] = HashTable(level, dim, grid_type, 0);
-for i=1:size(inv1,2)
-  coord = inv1{i};
-  filename = sprintf(out_format, i);
-  save(filename, 'coord')
-end
-
-
-out_format = strcat(element_dir, "element_table_2_3_SG_%d.dat");
-level = 3;
-dim = 2;
-grid_type = 'SG';
-[fwd2, inv2] = HashTable(level, dim, grid_type, 0);
-for i=1:size(inv2,2)
-  coord = inv2{i};
-  filename = sprintf(out_format, i);
-  save(filename, 'coord')
-end
-
-out_format = strcat(element_dir, "element_table_3_4_FG_%d.dat");
-level = 4;
-dim = 3;
-grid_type = 'FG';
-[fwd3, inv3] = HashTable(level, dim, grid_type, 0);
-for i=1:size(inv3,2)
-  coord = inv3{i};
-  filename = sprintf(out_format, i);
-  save(filename, 'coord');
-end
-
-clear
-
 % permutations testing files
 permutations_dir = strcat(pwd, "/", "generated-inputs", "/", "permutations", "/");
 mkdir (permutations_dir);
