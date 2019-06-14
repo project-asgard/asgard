3520ec8042b63b31efb4d866cda3e41a45be8e43
David Green <david.lindsay.green@gmail.com>
2019-06-14 11:19:14 -0400

    matlab responsible for the connectivity test data

--- testing/scripts/generate_test_data.m
+++ testing/scripts/generate_test_data.m
@@ -15,60 +15,6 @@ addpath(genpath(pwd));
 warning('on');
 % write output files for each component
 
-% connectivity testing files
-connectivity_dir = strcat(pwd, "/", "generated-inputs", "/", "connectivity", "/");
-mkdir (connectivity_dir);
-
-% 1d indexing
-out_format = strcat(connectivity_dir, "get_1d_%d_%d.dat");
-levs = [0, 0, 5];
-cells = [0, 1, 9];
-for i=1:size(levs,2)
-index = LevCell2index(levs(i), cells(i));
-filename = sprintf(out_format, levs(i), cells(i));
-save(filename, 'index');
-end
-
-% 1d connectivity
-out_format = strcat(connectivity_dir, "connect_1_%d.dat");
-levs = [1, 2, 8];
-for i=1:size(levs,2)
-connectivity = full(Connect1D(levs(i)));
-filename = sprintf(out_format, levs(i));
-save(filename, 'connectivity');
-end
-
-% nd connectivity
-out_format = strcat(connectivity_dir, "connect_n_2_3_FG_%d.dat");
-dims = 2;
-levs = 3;
-grid = 'FG';
-lev_sum = 6;
-lev_max = 3;
-[fwd, rev] = HashTable(levs, dims, grid, 1);
-connectivity = ConnectnD(dims, fwd, rev, lev_sum, lev_max);
-for i=1:size(connectivity, 2)
-filename = sprintf(out_format, i);
-element = connectivity{i};
-save(filename, 'element');
-end
-
-out_format = strcat(connectivity_dir, "connect_n_3_4_SG_%d.dat");
-dims = 3;
-levs = 4;
-grid = 'SG';
-lev_sum = 4;
-lev_max = 4;
-[fwd, rev] = HashTable(levs, dims, grid, 1);
-connectivity = ConnectnD(dims, fwd, rev, lev_sum, lev_max);
-for i=1:size(connectivity, 2)
-filename = sprintf(out_format, i);
-element = connectivity{i};
-save(filename, 'element');
-end
-
-clear
-
 % matlab testing
 
 matlab_dir = strcat(pwd, "/", "generated-inputs", "/", "matlab_utilities", "/");
