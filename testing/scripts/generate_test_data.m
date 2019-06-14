330002f47fa13df37e963eaa876089135811db19
David Green <david.lindsay.green@gmail.com>
2019-06-14 12:40:37 -0400

    matlab responsible for the matlab_utilities testing data

--- testing/scripts/generate_test_data.m
+++ testing/scripts/generate_test_data.m
@@ -15,54 +15,6 @@ addpath(genpath(pwd));
 warning('on');
 % write output files for each component
 
-% matlab testing
-
-matlab_dir = strcat(pwd, "/", "generated-inputs", "/", "matlab_utilities", "/");
-mkdir (matlab_dir);
-
-% these are used to test linspace()
-
-out_format = strcat(matlab_dir, "linspace_");
-w1 = linspace(-1,1,9);
-w2 = linspace(1,-1,9);
-w3 = linspace(-1,1,8);
-save(strcat(out_format,'neg1_1_9.dat'), 'w1');
-save(strcat(out_format,'1_neg1_9.dat'), 'w2');
-save(strcat(out_format,'neg1_1_8.dat'), 'w3');
-
-% these are used to test read_vector_from_bin_file()
-
-out_format = strcat(matlab_dir, "read_vector_bin_");
-w = linspace(-1,1);
-wT = w';
-writeToFile(strcat(out_format, 'neg1_1_100.dat'), w);
-writeToFile(strcat(out_format, 'neg1_1_100T.dat'), wT);
-
-% these are used to test read_vector_from_txt_file()
-
-out_format = strcat(matlab_dir, "read_vector_txt_");
-w2 = linspace(-1,1);
-w2T = w';
-save(strcat(out_format, 'neg1_1_100.dat'), 'w2');
-save(strcat(out_format, 'neg1_1_100T.dat'), 'w2T');
-
-% these are used to test read_vector_from_txt_file()
-
-for i = 0:4; for j = 0:4
-  m(i+1,j+1) = 17/(i+1+j);
-  %endfor; endfor;
-end
-end
-
-save(strcat(matlab_dir, 'read_matrix_txt_5x5.dat'), 'm');
-
-% test read_scalar_from_txt_file()
-
-s = 42;
-save(strcat(matlab_dir, 'read_scalar_42.dat'), 's');
-
-clear
-
 % quadrature testing
 
 quad_dir = strcat(pwd, "/", "generated-inputs", "/", "quadrature", "/");
