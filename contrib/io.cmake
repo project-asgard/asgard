0a13e447ffe34c34add553edc7cbbe740a8cd226
Graham Lopez <lopezmg@ornl.gov>
2019-06-18 15:36:56 -0400

    Half-built HighFive, but bailing
    Since it is Boost licensed and we already include other Boost licensed
    software in the repo, I'm going to do the same here. I'm tired of trying
    to figure out how to install it on the fly.

--- contrib/io.cmake
+++ contrib/io.cmake
@@ -1,1 +1,96 @@
-find_package(HighFive REQUIRED)
+###############################################################################
+## IO-related support
+###############################################################################
+
+###############################################################################
+## HDF5 (libhdf5) (https://portal.hdfgroup.org/display/support)
+#
+# right now, we only access this through the HighFive wrapper lib
+###############################################################################
+function (get_hdf5)
+  if (NOT ASGARD_BUILD_HDF5)
+    # search for hdf5 under user-supplied path(s)
+    if (ASGARD_HDF5_PATH)
+      find_library(HDF5_LIB_PATH libhdf5 PATHS ${ASGARD_HDF5_PATH}
+        PATH_SUFFIXES lib NO_DEFAULT_PATH)
+      if (HDF5_LIB_PATH)
+        set (hdf5_PATH ${HDF5_LIB_PATH})
+      endif ()
+    endif ()
+
+    # search for hdf5 in some typical locations
+    if (NOT hdf5_PATH)
+      find_library (HDF5_LIB_PATH libhdf5 PATHS /usr/ /usr/local/
+        PATH_SUFFIXES lib lib64 NO_DEFAULT_PATH)
+      if (HDF5_LIB_PATH)
+        set (hdf5_PATH ${HDF5_LIB_PATH})
+      endif ()
+    endif ()
+
+    if (hdf5_PATH)
+      message (STATUS "using external hdf5 found at ${hdf5_PATH}")
+    endif ()
+  endif ()
+
+  # if cmake couldn't find other hdf5, or the user asked to build it
+  if (NOT hdf5_PATH)
+    set (hdf5_PATH ${CMAKE_SOURCE_DIR}/contrib/hdf5)
+      if (NOT EXISTS ${hdf5_PATH}/lib/libhdf5.so)
+        message (STATUS "libhdf5 not found. We'll build it from source.")
+
+        include (ExternalProject)
+        ExternalProject_Add (hdf5-ext
+          UPDATE_COMMAND ""
+          PREFIX contrib/io/hdf5
+          URL https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-1.10.5/src/hdf5-1.10.5.tar.bz2
+          DOWNLOAD_NO_PROGRESS 1
+          CONFIGURE_COMMAND ${CMAKE_CURRENT_BINARY_DIR}/contrib/io/hdf5/src/hdf5-ext/configure --prefix=${hdf5_PATH}
+          BUILD_COMMAND make
+          BUILD_IN_SOURCE 1
+          INSTALL_COMMAND make install
+        )
+      else ()
+        message (STATUS "using contrib HDF5 found at ${hdf5_PATH}")
+      endif ()
+  endif ()
+endfunction()
+
+###############################################################################
+## BlueBrain/HighFive (https://github.com/BlueBrain/HighFive)
+#
+# header-only library for a c++ interface into libhdf5
+###############################################################################
+if (ASGARD_IO_HIGHFIVE)
+
+  get_hdf5()
+
+  # search for highfive under user-supplied path(s)
+  if (ASGARD_HIGHFIVE_PATH)
+    find_package (HighFive QUIET PATHS ${ASGARD_HIGHFIVE_PATH})
+  endif ()
+
+  # if cmake couldn't find other highfive
+  if (NOT HighFive_FOUND)
+    # see if it is already in contrib
+    set (HighFive_contrib_PATH ${CMAKE_SOURCE_DIR}/contrib/HighFive)
+    find_package (HighFive QUIET PATHS ${HighFive_contrib_PATH})
+    if (NOT HighFive_FOUND)
+      message (STATUS "HighFive not found. We'll build it from source.")
+
+      include (ExternalProject)
+      ExternalProject_Add (HighFive
+        UPDATE_COMMAND ""
+        PREFIX contrib/HighFive
+        GIT_REPOSITORY https://github.com/BlueBrain/HighFive
+        GIT_PROGRESS 1
+        GIT_SHALLOW 1
+        set (INSTALL_DIR ${SOURCE_DIR})
+        CONFIGURE_COMMAND cmake -DUSE_BOOST=0
+        BUILD_COMMAND rm -rf ${HighFive_contrib_PATH}
+        INSTALL_COMMAND mv ${CMAKE_CURRENT_BINARY_DIR}/contrib/HighFive/src/HighFive ${HighFive_contrib_PATH}
+      )
+    else ()
+      message (STATUS "using contrib HighFive found at ${HighFive_ROOT}")
+    endif ()
+  endif()
+endif()
