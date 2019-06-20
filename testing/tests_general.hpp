69f75f0b19ba6e2c79bb66bc2bfd82d6d8d47434
adam-mcdaniel <adam.mcdaniel17@gmail.com>
2019-06-20 15:03:21 -0400

    Added initial_diff var in tests_general for readability

--- testing/tests_general.hpp
+++ testing/tests_general.hpp
@@ -144,30 +144,33 @@ auto const cons_reduce_comparison(F const transform)
 
 // Confirm the difference between two iterators is below a tolerance
 template<typename T>
-auto const diff_comparison = [](auto const &first_iter,
-                                auto const &second_iter) {
-  // Create a function that will accumulate the greatest difference
-  // for each pair in the zipped iterators first_iter and second_iter
-  T const result = std::abs(cons_reduce_comparison(
-      [](auto const &first, auto const &second, auto const accumulator) {
-        auto const diff = std::abs(first - second);
-        if (diff > accumulator)
-        {
-          return diff;
-        }
-        return accumulator;
-      })(first_iter, second_iter, *first_iter.begin() - *second_iter.begin()));
+auto const
+    diff_comparison = [](auto const &first_iter, auto const &second_iter) {
+      // The difference between the first objects in each iter
+      const auto initial_diff = *first_iter.begin() - *second_iter.begin();
 
-  // Return whether or not the difference is greater than the tolerance
-  if constexpr (std::is_same<T, double>::value)
-  {
-    T const tol = std::numeric_limits<T>::epsilon() * 1e5;
-    return result <= tol;
-  }
-  else
-  {
-    T const tol = std::numeric_limits<T>::epsilon() * 1e3;
-    return result <= tol;
-  }
-};
+      // Create a function that will accumulate the greatest difference
+      // for each pair in the zipped iterators first_iter and second_iter
+      T const result = std::abs(cons_reduce_comparison(
+          [](auto const &first, auto const &second, auto const accumulator) {
+            auto const diff = std::abs(first - second);
+            if (diff > accumulator)
+            {
+              return diff;
+            }
+            return accumulator;
+          })(first_iter, second_iter, initial_diff));
+
+      // Return whether or not the difference is greater than the tolerance
+      if constexpr (std::is_same<T, double>::value)
+      {
+        T const tol = std::numeric_limits<T>::epsilon() * 1e5;
+        return result <= tol;
+      }
+      else
+      {
+        T const tol = std::numeric_limits<T>::epsilon() * 1e3;
+        return result <= tol;
+      }
+    };
 #endif
