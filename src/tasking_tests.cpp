2e423d807812c1f3387b3192b3d83ce50b2e1a34
Benjamin T. McDaniel <bmcdan16@utk.edu>
2019-07-03 10:05:19 -0400

    Add initial test

--- src/tasking_tests.cpp
+++ src/tasking_tests.cpp
@@ -2,6 +2,34 @@
 #include "tasking.hpp"
 #include "tests_general.hpp"
 
+// check for complete, non-overlapping element assignment
+auto const validity_check = [](std::vector<task> const &tasks,
+                               int const num_elems) {
+  enum element_status
+  {
+    unassigned,
+    assigned
+  };
+  fk::matrix<element_status> coverage(num_elems, num_elems);
+  for (task const &task : tasks)
+  {
+    for (int i = task.elem_start; i <= task.elem_end; ++i)
+    {
+      for (int j = task.conn_start; j <= task.conn_end; ++j)
+      {
+        // non-overlapping check
+        REQUIRE(coverage(i, j) == element_status::unassigned);
+        coverage(i, j) = element_status::assigned;
+      }
+    }
+  }
+  for (element_status const &status : coverage)
+  {
+    // complete check
+    REQUIRE(status == element_status::assigned);
+  }
+};
+
 TEST_CASE("tasking list generation", "[tasking]")
 {
   SECTION("1 rank, 1 MB")
@@ -20,7 +48,7 @@ TEST_CASE("tasking list generation", "[tasking]")
 
     int const num_tasks = get_num_tasks(table, *pde, ranks, MB);
     auto const tasks    = assign_elements_to_tasks(table, num_tasks);
-    assert(tasks.size() == num_tasks);
-    REQUIRE(true);
+    assert(static_cast<int>(tasks.size()) == num_tasks);
+    validity_check(tasks, table.size());
   }
 }
