e5b165dd8ac26f65c569ca2cf00d2fec6baa8ec1
Benjamin T. McDaniel <bmcdan16@utk.edu>
2019-08-12 14:56:05 -0400

    Add mapping from assigned chunks to input/output vectors

--- src/plan.cpp
+++ src/plan.cpp
@@ -8,3 +8,116 @@ get_my_chunks(std::vector<element_chunk> const &all_chunks, int const my_rank,
 rank_to_inputs get_my_inputs(std::vector<element_chunk> const &my_chunks,
                              int const my_rank, int const all_ranks)
 {}
+
+static bool is_contiguous(std::vector<element_chunk> const &chunks)
+{
+  enum element_status
+  {
+    unassigned,
+    assigned
+  };
+
+  int total_rows = 0;
+  int total_cols = 0;
+  for (auto const &chunk : chunks)
+  {
+    limits<> rows = rows_in_chunk(chunk);
+    limits<> cols = columns_in_chunk(chunk);
+    total_rows += rows.stop - rows.start + 1;
+    total_cols += cols.stop - cols.start + 1;
+  }
+
+  fk::matrix<element_status> coverage(total_rows, total_cols);
+  for (auto const &chunk : chunks)
+  {
+    for (auto const &[row, cols] : chunk)
+    {
+      for (int col = cols.start; col <= cols.stop; ++col)
+      {
+        if (coverage(row, col) != element_status::unassigned)
+        {
+          return false;
+        }
+        coverage(row, col) = element_status::assigned;
+      }
+    }
+  }
+  for (auto const &elem : coverage)
+  {
+    if (elem != element_status::assigned)
+    {
+      return false;
+    }
+  }
+  return true;
+}
+
+template<typename P>
+limits<int64_t>
+get_input_range(std::vector<element_chunk> const &my_chunks, PDE<P> const &pde)
+{
+  assert(is_contiguous(my_chunks));
+
+  int64_t const first_col =
+      columns_in_chunk(
+          *std::min_element(my_chunks.begin(), my_chunks.end(),
+                            [](const element_chunk &a, const element_chunk &b) {
+                              return columns_in_chunk(a).start <
+                                     columns_in_chunk(b).start;
+                            }))
+          .start;
+
+  int64_t const last_col =
+      columns_in_chunk(
+          *std::max_element(my_chunks.begin(), my_chunks.end(),
+                            [](const element_chunk &a, const element_chunk &b) {
+                              return columns_in_chunk(a).start <
+                                     columns_in_chunk(b).start;
+                            }))
+          .stop;
+
+  return limits<int64_t>(first_col * element_segment_size(pde),
+                         last_col * element_segment_size(pde));
+}
+
+template<typename P>
+limits<int64_t>
+get_output_range(std::vector<element_chunk> const &my_chunks, PDE<P> const &pde)
+{
+  assert(is_contiguous(my_chunks));
+
+  int64_t const first_row =
+      rows_in_chunk(
+          *std::min_element(my_chunks.begin(), my_chunks.end(),
+                            [](const element_chunk &a, const element_chunk &b) {
+                              return rows_in_chunk(a).start <
+                                     rows_in_chunk(b).start;
+                            }))
+          .start;
+
+  int64_t const last_row =
+      columns_in_chunk(
+          *std::max_element(my_chunks.begin(), my_chunks.end(),
+                            [](const element_chunk &a, const element_chunk &b) {
+                              return rows_in_chunk(a).start <
+                                     rows_in_chunk(b).start;
+                            }))
+          .stop;
+
+  return limits<int64_t>(first_row * element_segment_size(pde),
+                         last_row * element_segment_size(pde));
+}
+
+template limits<int64_t>
+get_input_range(std::vector<element_chunk> const &my_chunks,
+                PDE<float> const &pde);
+template limits<int64_t>
+get_input_range(std::vector<element_chunk> const &my_chunks,
+                PDE<double> const &pde);
+
+template limits<int64_t>
+get_output_range(std::vector<element_chunk> const &my_chunks,
+                 PDE<float> const &pde);
+template limits<int64_t>
+get_output_range(std::vector<element_chunk> const &my_chunks,
+                 PDE<double> const &pde);
