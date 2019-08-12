25580efb48cc5d29eb06f0958eb184ece2815753
Benjamin T. McDaniel <bmcdan16@utk.edu>
2019-08-12 15:13:21 -0400

    task splitting function

--- src/plan.cpp
+++ src/plan.cpp
@@ -2,12 +2,16 @@
 
 std::vector<element_chunk>
 get_my_chunks(std::vector<element_chunk> const &all_chunks, int const my_rank,
-              int const all_ranks)
-{}
+              int const num_ranks)
+{
+  assert(all_chunks.size() % num_ranks == 0);
+  assert(my_rank < num_ranks);
 
-rank_to_inputs get_my_inputs(std::vector<element_chunk> const &my_chunks,
-                             int const my_rank, int const all_ranks)
-{}
+  int const chunks_per_rank = all_chunks.size() / num_ranks;
+  auto const my_first       = all_chunks.cbegin() + chunks_per_rank * my_rank;
+  auto const my_last = all_chunks.cbegin() + chunks_per_rank * (my_rank + 1);
+  return std::vector<element_chunk>(my_first, my_last);
+}
 
 static bool is_contiguous(std::vector<element_chunk> const &chunks)
 {
@@ -52,6 +56,13 @@ static bool is_contiguous(std::vector<element_chunk> const &chunks)
   return true;
 }
 
+rank_to_inputs get_my_inputs(std::vector<element_chunk> const &my_chunks,
+                             int const my_rank, int const num_ranks)
+{
+  assert(my_rank < num_ranks);
+  assert(is_contiguous(my_chunks));
+}
+
 template<typename P>
 limits<int64_t>
 get_input_range(std::vector<element_chunk> const &my_chunks, PDE<P> const &pde)
@@ -71,8 +82,8 @@ get_input_range(std::vector<element_chunk> const &my_chunks, PDE<P> const &pde)
       columns_in_chunk(
           *std::max_element(my_chunks.begin(), my_chunks.end(),
                             [](const element_chunk &a, const element_chunk &b) {
-                              return columns_in_chunk(a).start <
-                                     columns_in_chunk(b).start;
+                              return columns_in_chunk(a).stop <
+                                     columns_in_chunk(b).stop;
                             }))
           .stop;
 
@@ -99,8 +110,8 @@ get_output_range(std::vector<element_chunk> const &my_chunks, PDE<P> const &pde)
       columns_in_chunk(
           *std::max_element(my_chunks.begin(), my_chunks.end(),
                             [](const element_chunk &a, const element_chunk &b) {
-                              return rows_in_chunk(a).start <
-                                     rows_in_chunk(b).start;
+                              return rows_in_chunk(a).stop <
+                                     rows_in_chunk(b).stop;
                             }))
           .stop;
 
