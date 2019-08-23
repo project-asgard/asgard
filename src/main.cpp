46eb30cde6fc57b6ecb4988482c8eff4d6392311
Benjamin T. McDaniel <bmcdan16@utk.edu>
2019-08-23 14:51:08 -0400

    wrap couts in main to print once per node

--- src/main.cpp
+++ src/main.cpp
@@ -28,18 +28,17 @@ int main(int argc, char **argv)
 #ifdef ASGARD_USE_MPI
   auto const init_status = MPI_Init(&argc, &argv);
   assert(init_status == 0);
-
 #endif
 
-  std::cout << "Branch: " << GIT_BRANCH << '\n';
-  std::cout << "Commit Summary: " << GIT_COMMIT_HASH << GIT_COMMIT_SUMMARY
-            << '\n';
-  std::cout << "This executable was built on " << BUILD_TIME << '\n';
+  node_out() << "Branch: " << GIT_BRANCH << '\n';
+  node_out() << "Commit Summary: " << GIT_COMMIT_HASH << GIT_COMMIT_SUMMARY
+             << '\n';
+  node_out() << "This executable was built on " << BUILD_TIME << '\n';
 
   options opts(argc, argv);
 
   // -- parse user input and generate pde
-  std::cout << "generating: pde..." << '\n';
+  node_out() << "generating: pde..." << '\n';
   auto pde = make_PDE<prec>(opts.get_selected_pde(), opts.get_level(),
                             opts.get_degree());
 
@@ -52,39 +51,39 @@ int main(int argc, char **argv)
   // if we ever do go to p-adaptivity (variable degree) we can change it then
   auto const degree = pde->get_dimensions()[0].get_degree();
 
-  std::cout << "ASGarD problem configuration:" << '\n';
-  std::cout << "  selected PDE: " << opts.get_pde_string() << '\n';
-  std::cout << "  level: " << opts.get_level() << '\n';
-  std::cout << "  degree: " << opts.get_degree() << '\n';
-  std::cout << "  N steps: " << opts.get_time_steps() << '\n';
-  std::cout << "  write freq: " << opts.get_write_frequency() << '\n';
-  std::cout << "  vis. freq: " << opts.get_visualization_frequency() << '\n';
-  std::cout << "  implicit: " << opts.using_implicit() << '\n';
-  std::cout << "  full grid: " << opts.using_full_grid() << '\n';
-  std::cout << "  CFL number: " << opts.get_cfl() << '\n';
-  std::cout << "  Poisson solve: " << opts.do_poisson_solve() << '\n';
+  node_out() << "ASGarD problem configuration:" << '\n';
+  node_out() << "  selected PDE: " << opts.get_pde_string() << '\n';
+  node_out() << "  level: " << opts.get_level() << '\n';
+  node_out() << "  degree: " << opts.get_degree() << '\n';
+  node_out() << "  N steps: " << opts.get_time_steps() << '\n';
+  node_out() << "  write freq: " << opts.get_write_frequency() << '\n';
+  node_out() << "  vis. freq: " << opts.get_visualization_frequency() << '\n';
+  node_out() << "  implicit: " << opts.using_implicit() << '\n';
+  node_out() << "  full grid: " << opts.using_full_grid() << '\n';
+  node_out() << "  CFL number: " << opts.get_cfl() << '\n';
+  node_out() << "  Poisson solve: " << opts.do_poisson_solve() << '\n';
 
   // -- print out time and memory estimates based on profiling
   std::pair<std::string, double> runtime_info = expected_time(
       opts.get_selected_pde(), opts.get_level(), opts.get_degree());
-  std::cout << "Predicted compute time (seconds): " << runtime_info.second
-            << '\n';
-  std::cout << runtime_info.first << '\n';
+  node_out() << "Predicted compute time (seconds): " << runtime_info.second
+             << '\n';
+  node_out() << runtime_info.first << '\n';
 
   std::pair<std::string, double> mem_usage_info = total_mem_usage(
       opts.get_selected_pde(), opts.get_level(), opts.get_degree());
-  std::cout << "Predicted total mem usage (MB): " << mem_usage_info.second
-            << '\n';
-  std::cout << mem_usage_info.first << '\n';
+  node_out() << "Predicted total mem usage (MB): " << mem_usage_info.second
+             << '\n';
+  node_out() << mem_usage_info.first << '\n';
 
-  std::cout << "--- begin setup ---" << '\n';
+  node_out() << "--- begin setup ---" << '\n';
 
   // -- create forward/reverse mapping between elements and indices
-  std::cout << "  generating: element table..." << '\n';
+  node_out() << "  generating: element table..." << '\n';
   element_table const table = element_table(opts, pde->num_dims);
 
   // -- generate initial condition vector.
-  std::cout << "  generating: initial conditions..." << '\n';
+  node_out() << "  generating: initial conditions..." << '\n';
   fk::vector<prec> const initial_condition = [&pde, &table, degree]() {
     std::vector<fk::vector<prec>> initial_conditions;
     for (dimension<prec> const &dim : pde->get_dimensions())
@@ -103,7 +102,7 @@ int main(int argc, char **argv)
   // -- generate source vectors.
   // these will be scaled later according to the simulation time applied
   // with their own time-scaling functions
-  std::cout << "  generating: source vectors..." << '\n';
+  node_out() << "  generating: source vectors..." << '\n';
   std::vector<fk::vector<prec>> const initial_sources = [&pde, &table,
                                                          degree]() {
     std::vector<fk::vector<prec>> initial_sources;
@@ -124,7 +123,7 @@ int main(int argc, char **argv)
   }();
 
   // -- generate analytic solution vector.
-  std::cout << "  generating: analytic solution at t=0 ..." << '\n';
+  node_out() << "  generating: analytic solution at t=0 ..." << '\n';
   fk::vector<prec> const analytic_solution = [&pde, &table, degree]() {
     std::vector<fk::vector<prec>> analytic_solutions_D;
     for (int d = 0; d < pde->num_dims; d++)
@@ -136,15 +135,16 @@ int main(int argc, char **argv)
   }();
 
   // -- generate and store coefficient matrices.
-  std::cout << "  generating: coefficient matrices..." << '\n';
 
+  std::cout << "  generating: coefficient matrices..." << '\n';
+  
   generate_all_coefficients<prec>(*pde);
 
   // this is to bail out for further profiling/development on the setup routines
   if (opts.get_time_steps() < 1)
     return 0;
 
-  std::cout << "--- begin time loop staging ---" << '\n';
+  node_out() << "--- begin time loop staging ---" << '\n';
   // -- allocate/setup for batch gemm
   auto const get_MB = [&](int num_elems) {
     uint64_t const bytes   = num_elems * sizeof(prec);
@@ -169,27 +169,27 @@ int main(int argc, char **argv)
       table, get_num_chunks(table, *pde, ranks, default_workspace_MB));
   rank_workspace<prec> rank_space(*pde, chunks);
 
-  std::cout << "allocating workspace..." << '\n';
+  node_out() << "allocating workspace..." << '\n';
 
-  std::cout << "input vector size (MB): "
-            << get_MB(rank_space.batch_input.size()) << '\n';
-  std::cout << "kronmult output space size (MB): "
-            << get_MB(rank_space.reduction_space.size()) << '\n';
-  std::cout << "kronmult working space size (MB): "
-            << get_MB(rank_space.batch_intermediate.size()) << '\n';
-  std::cout << "output vector size (MB): "
-            << get_MB(rank_space.batch_output.size()) << '\n';
+  node_out() << "input vector size (MB): "
+             << get_MB(rank_space.batch_input.size()) << '\n';
+  node_out() << "kronmult output space size (MB): "
+             << get_MB(rank_space.reduction_space.size()) << '\n';
+  node_out() << "kronmult working space size (MB): "
+             << get_MB(rank_space.batch_intermediate.size()) << '\n';
+  node_out() << "output vector size (MB): "
+             << get_MB(rank_space.batch_output.size()) << '\n';
   auto const &unit_vect = rank_space.get_unit_vector();
-  std::cout << "reduction vector size (MB): " << get_MB(unit_vect.size())
-            << '\n';
+  node_out() << "reduction vector size (MB): " << get_MB(unit_vect.size())
+             << '\n';
 
-  std::cout << "explicit time loop workspace size (host) (MB): "
-            << host_space.size_MB() << '\n';
+  node_out() << "explicit time loop workspace size (host) (MB): "
+             << host_space.size_MB() << '\n';
 
   host_space.x = initial_condition;
 
   // -- time loop
-  std::cout << "--- begin time loop ---" << '\n';
+  node_out() << "--- begin time loop ---" << '\n';
   prec const dt = pde->get_dt() * opts.get_cfl();
   for (int i = 0; i < opts.get_time_steps(); ++i)
   {
@@ -224,9 +224,9 @@ int main(int argc, char **argv)
         return std::sqrt(mean);
       }();
       auto const relative_error = RMSE / inf_norm(analytic_solution_t) * 100;
-      std::cout << "RMSE (numeric-analytic) [wavelet]: " << RMSE << '\n';
-      std::cout << "Relative difference (numeric-analytic) [wavelet]: "
-                << relative_error << " %" << '\n';
+      node_out() << "RMSE (numeric-analytic) [wavelet]: " << RMSE << '\n';
+      node_out() << "Relative difference (numeric-analytic) [wavelet]: "
+                 << relative_error << " %" << '\n';
     }
 
     // write output to file
@@ -234,10 +234,10 @@ int main(int argc, char **argv)
     update_output_file(output_dataset, host_space.fx);
 #endif
 
-    std::cout << "timestep: " << i << " complete" << '\n';
+    node_out() << "timestep: " << i << " complete" << '\n';
   }
 
-  std::cout << "--- simulation complete ---" << '\n';
+  node_out() << "--- simulation complete ---" << '\n';
 
 #ifdef ASGARD_USE_MPI
   auto const final_status = MPI_Finalize();
