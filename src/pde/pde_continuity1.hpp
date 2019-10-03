8711ef80418392760e8e2da042be190446c4705e
Tyler McDaniel <bmcdan16@utk.edu>
2019-10-03 11:55:47 -0400

    remove clang-format tags

--- src/pde/pde_continuity1.hpp
+++ src/pde/pde_continuity1.hpp
@@ -121,14 +121,9 @@ private:
 
   inline static std::vector<dimension<P>> const dimensions_ = {dim0_};
 
-  // clang-format off
-  inline static const partial_term<P> 
-  partial_term_0 = partial_term<P>( coefficient_type::grad,
-                                    g_func_0,
-                                    flux_type::central,
-                                    boundary_condition::periodic,
-                                    boundary_condition::periodic );
-  // clang-format on
+  inline static const partial_term<P> partial_term_0 = partial_term<P>(
+      coefficient_type::grad, g_func_0, flux_type::central,
+      boundary_condition::periodic, boundary_condition::periodic);
 
   // define terms (1 in this case)
   inline static term<P> term0_dim0_ =
