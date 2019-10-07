e3be2ccf3ad9a4f1063feb18d09cdbbdc3faa058
Tim Younkin <tyounkin19@gmail.com>
2019-10-07 14:29:49 -0400

    cleaned up element table files and clang-formatted

--- src/element_table_tests.cpp
+++ src/element_table_tests.cpp
@@ -35,47 +35,25 @@ TEST_CASE("element table constructor/accessors/size", "[element_table]")
     int const dims_2   = 2;
     options o_2        = make_options({"-l", std::to_string(levels_2)});
     element_table<int> t_2(o_2, dims_2);
-    //for(auto it = t_2.forward_table.cbegin(); it != t_2.forward_table.cend(); ++it)
-    //{
-    //  fk::vector<int> first = it->first;
-    //  first.print("first");
-    //  std::cout << it->second  << "\n";
-    //}
-    //for (std::vector<fk::vector<int>>::iterator it = t_2.reverse_table.begin() ; it != t_2.reverse_table.end(); ++it)
-    //{
-    //  fk::vector<int> thisvec =  *it;
-    //  thisvec.print("thisvec");
-    //}
     std::string test_base = out_base + "_2_3_SG";
     std::string file_path = test_base + ".dat";
     auto gold = fk::matrix<int>(read_matrix_from_txt_file(file_path));
-    
+
     std::string test_base_ind = out_base + "_index_2_3_SG";
     std::string file_path_ind = test_base_ind + ".dat";
-    //std::cout << file_path_ind << std::endl;
     auto gold_ind = fk::matrix<int>(read_matrix_from_txt_file(file_path_ind));
-    //gold.print("gold");
-    //gold_ind.print("gold_ind");
     for (auto i = 0; i < t_2.size(); ++i)
     {
       fk::vector<int> gold_coords =
           gold.extract_submatrix(i, 0, 1, gold.ncols());
-      //std::cout << "t2.get_coods and gold_coords " <<  std::endl;
-      fk::vector<int> t2coords= t_2.get_coords(i);
-      //t2coords.print("t2coords");
-      //gold_coords.print("gold_coords");
+      fk::vector<int> t2coords = t_2.get_coords(i);
+
       REQUIRE(t_2.get_coords(i) == gold_coords);
-      int t2getinds= t_2.get_index(gold_coords);
-      fk::vector<int> indices = gold_ind.extract_submatrix(0,i,1,1);
-      int this_ind = indices(0);
-      //std::cout << "t2.get_index and i "<< t2getinds << " "  << this_ind << std::endl;
+      int t2getinds           = t_2.get_index(gold_coords);
+      fk::vector<int> indices = gold_ind.extract_submatrix(0, i, 1, 1);
+      int this_ind            = indices(0);
       REQUIRE(t_2.get_index(gold_coords) == this_ind);
     }
-    //for (auto i = 0; i < t_2.size(); ++i)
-    //{
-    //  fk::vector<int> t_3 = t_2.get_coords(i);
-    //  t_3.print("t3");
-    //}
     REQUIRE(t_2.size() == 20);
   }
 
@@ -88,20 +66,20 @@ TEST_CASE("element table constructor/accessors/size", "[element_table]")
     element_table<int> t_3(o_3, dims_3);
     std::string test_base = out_base + "_3_4_FG";
     std::string file_path = test_base + ".dat";
-    
+
     std::string test_base_ind = out_base + "_index_3_4_FG";
     std::string file_path_ind = test_base_ind + ".dat";
-    
-    auto gold = fk::matrix<int>(read_matrix_from_txt_file(file_path));
+
+    auto gold     = fk::matrix<int>(read_matrix_from_txt_file(file_path));
     auto gold_ind = fk::matrix<int>(read_matrix_from_txt_file(file_path_ind));
-    
+
     for (auto i = 0; i < t_3.size(); ++i)
     {
       fk::vector<int> gold_coords =
           gold.extract_submatrix(i, 0, 1, gold.ncols());
-      
-      fk::vector<int> indices = gold_ind.extract_submatrix(0,i,1,1);
-      int this_ind = indices(0);
+
+      fk::vector<int> indices = gold_ind.extract_submatrix(0, i, 1, 1);
+      int this_ind            = indices(0);
 
       REQUIRE(t_3.get_coords(i) == gold_coords);
       REQUIRE(t_3.get_index(gold_coords) == this_ind);
