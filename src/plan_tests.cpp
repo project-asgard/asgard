c3b94420c814f58cec35c17027d676bcd5ba3750
Tyler McDaniel <bmcdan16@utk.edu>
2019-10-10 14:16:46 -0400

    Remove vesitigal files

--- src/plan_tests.cpp
+++
@@ -1,8 +0,0 @@
-
-#include "plan.hpp"
-#include "tests_general.hpp"
-
-TEST_CASE("splitting chunks between ranks", "[plan]") {}
-
-TEMPLATE_TEST_CASE("mapping to input/output functions", "[plan]", float, double)
-{}
