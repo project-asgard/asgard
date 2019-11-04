f38f019a828afb6ac0726cebe95ecf666b6862c8
Harry Hughes <harrynh3@gmail.com>
2019-11-04 18:18:04 -0500

    Column-major version of the algorithm, requires fewer transposes

--- src/kron.hpp
+++ src/kron.hpp
@@ -1,31 +1,107 @@
 #include "tensors.hpp"
+#include "batch.hpp"
 #include "fast_math.hpp"
-#include "transformations.hpp"
+
+#include <numeric>
+#include <array>
 #include <iostream>
 
-template< typename P >
-fk::vector< P, mem_type::owner >
-kron( std::vector< fk::matrix< P, mem_type::owner > > const &matrix, 
-      fk::vector< P, mem_type::owner > const &x );
+template< typename P, mem_type mem, resource resrc >
+int calculate_workspace_len( std::vector< fk::matrix< P, mem, resrc > > const &matrix,
+                             int const x_size )
+{
+  int greatest = x_size;
+  int r_prod = 1;
+  int c_prod = 1;
 
-template< typename P >
-int calculate_workspace_len( std::vector< fk::matrix< P, mem_type::owner > > const &matrix,
-                             int const x_size );
+  typename std::vector< fk::matrix< P, mem, resrc > >::const_reverse_iterator iter;
 
-extern template
-fk::vector< double, mem_type::owner >
-kron( std::vector< fk::matrix< double, mem_type::owner > > const &matrix, 
-      fk::vector< double, mem_type::owner > const &x );
+  for( iter = matrix.rbegin(); iter != matrix.rend(); ++iter )
+  {
+    c_prod *= iter->ncols();
+    r_prod *= iter->nrows();
 
-extern template
-fk::vector< float, mem_type::owner >
-kron( std::vector< fk::matrix< float, mem_type::owner > > const &matrix, 
-      fk::vector< float, mem_type::owner > const &x );
+    int const size = x_size / c_prod * r_prod;
+    if( size > greatest ) greatest = size;
+  }
 
-extern template
-int calculate_workspace_len( std::vector< fk::matrix< float, mem_type::owner > > 
-                             const &matrix, int const x_size );
+  return greatest;
+}
 
-extern template
-int calculate_workspace_len( std::vector< fk::matrix< double, mem_type::owner > > 
-                             const &matrix, int const x_size );
+template< typename P, mem_type mem >
+fk::vector< P, mem_type::owner, resource::device >
+kron( std::vector< fk::matrix< P, mem, resource::device > > const &matrix, 
+      fk::vector< P, mem, resource::device > const &x )
+{
+  assert( matrix.size() > 0 );
+
+  /* ensure "x" is correct size */
+  int const x_size = std::accumulate( matrix.begin(), matrix.end(), 1, 
+                           []( int const i, fk::matrix< P, mem, resource::device > const &m )
+                           {
+                             return i * m.ncols();
+                           } );
+  assert( x.size() == x_size );
+
+  /* determine correct workspace length */
+  int const workspace_len = calculate_workspace_len( matrix, x_size );
+
+  /* Captain! Does this need to be on the device? */
+  /* set withk a view */
+  fk::vector<P, mem_type::owner, resource::device > w0(workspace_len);
+  w0.set_subvector( 0, x );
+  fk::vector<P, mem_type::owner, resource::device > w1(workspace_len);
+
+  std::array< fk::vector< P, mem_type::owner, resource::device >, 2 > workspace = { w0, w1 };
+
+  int in = 0;
+  int out = 1;
+  int stride = 1;
+  int v_size = x_size;
+  
+  typename std::vector< fk::matrix< P, mem, resource::device > >::const_reverse_iterator iter;
+  for( iter = matrix.rbegin(); iter != matrix.rend(); ++iter )
+  {
+    int const read_stride = stride * iter->ncols();
+    int const write_stride = stride * iter->nrows();
+    int const n_gemms = v_size / read_stride;
+
+    /* create a batch of matrices */
+    batch< P > left_side( n_gemms, stride, iter->ncols(), stride, false );
+    batch< P > right_side( n_gemms, iter->nrows(), 
+                                           iter->ncols(), iter->stride(), true );
+    batch< P > product( n_gemms, stride, iter->nrows(), stride, false );
+
+    for( int j = 0; j < n_gemms; ++j )
+    {
+      fk::matrix< P, mem, resource::device >
+      input( workspace[ in ], stride, iter->ncols(), j * read_stride );
+
+      fk::matrix< P, mem, resource::device >
+      output( workspace[ out ], stride, iter->nrows(), j * write_stride );
+
+      /* use an is_same statement here to figure out whether to use a view constructor or not */
+      left_side.assign_entry(input, j);
+      right_side.assign_entry( fk::matrix< P, mem_type::view, resource::device >( (*iter ) ), j );
+      product.assign_entry( output, j );
+    }
+    
+    /* new code */
+    batched_gemm< P >( left_side, right_side, product, 1, 0 );
+
+    v_size = n_gemms * write_stride;
+    stride = write_stride;
+
+    int const tmp = in;
+    in = out;
+    out = tmp;
+  }
+
+  int const y_size = std::accumulate( matrix.begin(), matrix.end(), 1, 
+                           []( int const i, fk::matrix< P, mem, resource::device > const &m )
+                           {
+                             return i * m.nrows();
+                           } );
+  
+  return workspace[in].extract(0, y_size - 1);
+}
--- src/kron_tests.cpp
+++ src/kron_tests.cpp
@@ -24,194 +24,98 @@ TEMPLATE_TEST_CASE( "kron", "[kron]", double, float )
     REQUIRE( calculate_workspace_len( matrix, x_size ) == correct_size );
   }
 
-  /* Captain! Run a test with an empty kron list and other edge cases */
-
   /* a simple 2 matrix and vector kron */
   SECTION("kron_0")
   {
-    fk::matrix< TestType > a =
+    fk::matrix< TestType, mem_type::owner, resource::device > a =
     { { 2, 3 }, { 4, 5 } };
 
-    fk::matrix< TestType > b =
+    fk::matrix< TestType, mem_type::owner, resource::device > b =
     { { 6, 7 }, { 8, 9 } };
 
-    std::vector< fk::matrix< TestType > > matrix = { a, b };
+    const std::vector< fk::matrix< TestType, mem_type::view, resource::device > > matrix =
+    { fk::matrix<TestType, mem_type::view, resource::device >(a),
+      fk::matrix<TestType, mem_type::view, resource::device >(b) };
 
-    fk::vector< TestType > x = { 10, 11, 12, 13 };
+    fk::vector< TestType, mem_type::owner, resource::device > x = { 10, 11, 12, 13 };
 
-    fk::vector< TestType > correct = { 763, 997, 1363, 1781 };
+    fk::vector< TestType, mem_type::owner, resource::host > correct = { 763, 997, 1363, 1781 };
 
-    fk::vector< TestType > r = kron( matrix, x );
+    fk::vector< TestType, mem_type::owner, resource::device > r = 
+    kron< TestType, mem_type::view >( matrix, 
+                                      fk::vector<TestType, mem_type::view, resource::device >(x) );
 
-    REQUIRE( r == correct );
+    REQUIRE( r.clone_onto_host() == correct );
   }
 
-  /* Just to check correct problem parameters */
   SECTION("kron_1")
   {
-    fk::matrix< TestType > a( 3, 8 );
-    fk::matrix< TestType > b( 5, 10 );
-    fk::matrix< TestType > c( 7, 4 );
-    fk::matrix< TestType > e( 9, 6 );
-    fk::matrix< TestType > f( 11, 12 );
+    auto matrix_all_twos =
+    []( int rows, int cols )-> fk::matrix< TestType, mem_type::owner, resource::device >
+    {
+      fk::matrix< TestType, mem_type::owner, resource::host> m( rows, cols );
 
-    std::vector< fk::matrix< TestType, mem_type::owner > > matrix =
-    { a, b, c, e, f };
+      for( int i = 0; i < rows; ++i )
+      {
+        for( int j = 0; j < cols; ++j )
+        {
+          m( i, j ) = 2;
+        }
+      }
 
+      return m.clone_onto_device();
+    };
+
+    auto m0 = matrix_all_twos( 3, 4 );
+    auto m1 = matrix_all_twos( 5, 3 );
+    auto m2 = matrix_all_twos( 2, 7 );
+    auto m3 = matrix_all_twos( 9, 6 );
+    auto m4 = matrix_all_twos( 12, 3 );
+    auto m5 = matrix_all_twos( 4, 14 );
+    auto m6 = matrix_all_twos( 10, 3 );
+    auto m7 = matrix_all_twos( 6, 5 );
+    fk::matrix<TestType, mem_type::owner, resource::device > m8 = { {3, 3}, {3, 3} };
+
+    std::vector< fk::matrix< TestType, mem_type::view, resource::device > > matrix =
+    { 
+      fk::matrix<TestType, mem_type::view, resource::device >( m0 ), 
+      fk::matrix<TestType, mem_type::view, resource::device >( m1 ),
+      fk::matrix<TestType, mem_type::view, resource::device >( m2 ),
+      fk::matrix<TestType, mem_type::view, resource::device >( m3 ),
+      fk::matrix<TestType, mem_type::view, resource::device >( m4 ),
+      fk::matrix<TestType, mem_type::view, resource::device >( m5 ),
+      fk::matrix<TestType, mem_type::view, resource::device >( m6 ),
+      fk::matrix<TestType, mem_type::view, resource::device >( m7 ),
+      fk::matrix<TestType, mem_type::view, resource::device >( m8 )
+    };
+
+    /* Captain! see if you can use auto to fix the compiler warning 
+       that will be generated below */
     int x_size = std::accumulate( matrix.begin(), matrix.end(), 1, 
-                                 []( int i, fk::matrix< TestType > &m )
+                                 []( int i, fk::matrix< TestType, mem_type::view > &m )
                                  {
                                    return i * m.ncols();
                                  } );
 
-    fk::vector< TestType > x( x_size );
-
-    /* Captain! Add a check for correct size */
-    //fk::vector< TestType > r = kron( matrix, x );
-  }
-
-  /* hacky ass way to test recursive kron correctness with existing routine */
-  SECTION("kron_2")
-  {
-    /* 2, 3 */
-    fk::matrix< TestType > a =
-    {
-    { 1, 1 }, { 1, 1 }
-    };
-    /* 5, 4 */
-    fk::matrix< TestType > b =
-    {
-    { 1, 1, 1, 1 }, { 1, 1, 1, 1 }, {1, 1, 1, 1 }, { 1, 1, 1, 1}, { 1, 1, 1, 1 }
-    };
-    /* 3, 7 */
-    fk::matrix< TestType > c =
-    {
-    { 1, 1, 1, 1, 1, 1, 1 }, { 1, 1, 1, 1, 1, 1, 1 }, { 1, 1, 1, 1, 1, 1, 1 }
-    };
-    /* 10, 2 */
-    fk::matrix< TestType > e =
-    {
-    { 1, 1 }, {1, 1}, { 1, 1 }, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, { 1, 1 }
-    };
-    /* 2, 6 */
-    fk::matrix< TestType > f =
-    {
-    {1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1}
-    };
-
-    std::vector< fk::matrix< TestType, mem_type::owner > > matrix =
-    { a, b, c, e, f };
-
-    std::vector< fk::matrix< TestType, mem_type::view > > matrix_views =
-    { fk::matrix< TestType, mem_type::view >( a ),
-      fk::matrix< TestType, mem_type::view >( b ),
-      fk::matrix< TestType, mem_type::view >( c ),
-      fk::matrix< TestType, mem_type::view >( e ),
-      fk::matrix< TestType, mem_type::view >( f ) };
-
-    int x_size = std::accumulate( matrix.begin(), matrix.end(), 1, 
-                                 []( int i, fk::matrix< TestType > &m )
+    int y_size = std::accumulate( matrix.begin(), matrix.end(), 1, 
+                                 []( int i, fk::matrix< TestType, mem_type::view > &m )
                                  {
-                                   return i * m.ncols();
+                                   return i * m.nrows();
                                  } );
 
-    std::vector< TestType > x_vec(x_size);
-    std::iota( x_vec.begin(), x_vec.end(), 1 );
-    fk::vector< TestType > x( x_vec );
+    /* could you make this a view instead of an owner? */
+    fk::vector< TestType, mem_type::owner, resource::host >
+    x( std::vector< TestType >( x_size, 1 ) );
 
-    fk::vector< TestType > r = kron( matrix, x );
-    fk::matrix< TestType > m = recursive_kron( matrix_views );
-    fk::vector< TestType > r_correct = m * x;
-    REQUIRE( r == r_correct );
-  }
+    auto r =
+    kron< TestType, mem_type::view >( matrix, 
+                                      fk::vector< TestType, 
+                                                  mem_type::view,
+                                                  resource::device >(x.clone_onto_device() ) );
 
-  SECTION("kron_3")
-  {
-    /* 2, 3 */
-    fk::matrix< TestType > a =
-    {
-    { 1, 2 }, { 3, 4 }
-    };
-    /* 5, 4 */
-    fk::matrix< TestType > b =
-    {
-    { 3, 6, 3, 6 }, { 9, 4, 7, 4 }, {6, 3, 4, 9 }, { 1, 3, 7, 11}, { 5, 6, 5, 1 }
-    };
-    /* 3, 7 */
-    fk::matrix< TestType > c =
-    {
-    { 1, 2, 3, 4, 5, 6, 7 }, { 7, 6, 4, 5, 2, 3, 1 }, { 8, 9, 7, 3, 5, 1, 8 }
-    };
-    /* 10, 2 */
-    fk::matrix< TestType > e =
-    {
-    { 7, 8 }, {7, 9}, { 8, 7 }, {3, 1}, {2, 5}, {8, 8}, {4, 6}, {7, 4}, {4, 5}, { 2, 3 }
-    };
+    fk::vector< TestType > 
+    correct( std::vector< TestType >( y_size, x_size * ( 1 << ( matrix.size() - 1 ) ) * 3 ) );
 
-    std::vector< fk::matrix< TestType, mem_type::owner > > matrix =
-    { a, b, c, e };
-
-    std::vector< fk::matrix< TestType, mem_type::view > > matrix_views =
-    { fk::matrix< TestType, mem_type::view >( a ),
-      fk::matrix< TestType, mem_type::view >( b ),
-      fk::matrix< TestType, mem_type::view >( c ),
-      fk::matrix< TestType, mem_type::view >( e ) };
-
-    int x_size = std::accumulate( matrix.begin(), matrix.end(), 1, 
-                                 []( int i, fk::matrix< TestType > &m )
-                                 {
-                                   return i * m.ncols();
-                                 } );
-
-    std::vector< TestType > x_vec(x_size);
-    std::iota( x_vec.begin(), x_vec.end(), 1 );
-    fk::vector< TestType > x( x_vec );
-
-    fk::vector< TestType > r = kron( matrix, x );
-    fk::matrix< TestType > m = recursive_kron( matrix_views );
-    fk::vector< TestType > r_correct = m * x;
-    REQUIRE( r == r_correct );
-  }
-
-  SECTION("kron_4")
-  {
-    fk::matrix< TestType > a =
-    {
-    { 4, 3 }, { 2, 5 }
-    };
-    fk::matrix< TestType > b =
-    {
-    { 6, 7 }, { 8, 9 }
-    };
-    fk::matrix< TestType > c =
-    {
-    { 2, 3 }, { 4, 5 }
-    };
-
-    std::vector< fk::matrix< TestType, mem_type::owner > > matrix =
-    { a, b, c };
-
-    std::vector< fk::matrix< TestType, mem_type::view > > matrix_views =
-    { fk::matrix< TestType, mem_type::view >( a ),
-      fk::matrix< TestType, mem_type::view >( b ),
-      fk::matrix< TestType, mem_type::view >( c ) };
-
-    int x_size = std::accumulate( matrix.begin(), matrix.end(), 1, 
-                                 []( int i, fk::matrix< TestType > &m )
-                                 {
-                                   return i * m.ncols();
-                                 } );
-
-    std::vector< TestType > x_vec(x_size);
-    std::iota( x_vec.begin(), x_vec.end(), 1 );
-    fk::vector< TestType > x( x_vec );
-
-    fk::vector< TestType > r = kron( matrix, x );
-    fk::matrix< TestType > m = recursive_kron( matrix_views );
-    fk::vector< TestType > r_correct = m * x;
-
-    r_correct.print();
-    r.print();
-    REQUIRE( r == r_correct );
+    REQUIRE( r.clone_onto_host() == correct );
   }
 }
