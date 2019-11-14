d7188b8690afa612a8cc0124cd39a88bc90afb91
Tyler McDaniel <bmcdan16@utk.edu>
2019-11-14 14:24:50 -0500

    io test refactor

--- src/io_tests.cpp
+++ src/io_tests.cpp
@@ -7,40 +7,33 @@ TEMPLATE_TEST_CASE("highfive interface to HDF5", "[io]", double, float, int)
 {
   SECTION("check it writes the correct file")
   {
-    // One problem with this test is that it will overwrite any existing
-    // "arsgard.h5" file in the present directory.
-
-    std::string const output_file_name("asgard.h5");
-    std::string const output_dataset_name("asgard");
+    std::string const output_file_name("asgard_test.h5");
+    std::string const output_dataset_name("asgard_test");
 
     // the golden values
-
-    std::vector<TestType> gold_svec1{1, 2, 3, 4, 5};
-    std::vector<TestType> gold_svec2{5, 6, 7, 8, 9};
-
-    fk::vector<TestType> gold_vec1 = gold_svec1;
-    fk::vector<TestType> gold_vec2 = gold_svec2;
+    std::vector<TestType> const gold_vec1{1, 2, 3, 4, 5};
+    std::vector<TestType> const gold_vec2{5, 6, 7, 8, 9};
 
     // setup output file and write initial condition
-
     auto output_dataset = initialize_output_file(gold_vec1);
 
     // write output to file
-
     update_output_file(output_dataset, gold_vec2);
 
     // now read back what we wrote out
-
     std::vector<std::vector<TestType>> read_data;
     output_dataset.read(read_data);
 
-    auto dataset_size = output_dataset.getDimensions();
-    auto vec1         = read_data[0];
-    auto vec2         = read_data[1];
-    for (int i = 0; i <= 4; i++)
+    auto const dataset_size = output_dataset.getDimensions();
+    auto const vec1         = read_data[0];
+    auto const vec2         = read_data[1];
+    REQUIRE(vec1.size() == gold_vec1.size());
+    REQUIRE(vec2.size() == gold_vec2.size());
+
+    for (int i = 0; i < static_cast<int>(vec.size()); i++)
     {
-      REQUIRE(vec1[i] == gold_svec1[i]);
-      REQUIRE(vec2[i] == gold_svec2[i]);
+      REQUIRE(vec1[i] == gold_vec1[i]);
+      REQUIRE(vec2[i] == gold_vec2[i]);
     }
   }
 }
