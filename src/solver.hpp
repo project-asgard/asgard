cbaad4a97958769f3faf92d9406e0a0725ca7d45
Tyler McDaniel <bmcdan16@utk.edu>
2020-03-10 13:59:49 -0400

    remove old code for merge

--- src/solver.hpp
+++ src/solver.hpp
@@ -8,17 +8,7 @@
 
 namespace solver
 {
-// perform GMRES using apply_A for tensor encoded matrix*vector
-// returns the residual
-// approximate solution stored in host_space.x
-template<typename P>
-P gmres(PDE<P> const &pde, element_table const &elem_table,
-        distribution_plan const &plan, std::vector<element_chunk> const &chunks,
-        host_workspace<P> &host_space, rank_workspace<P> &rank_space,
-        std::vector<batch_operands_set<P>> &batches, P const dt,
-        P const threshold, int const restart);
-
-// simple, node-local test version
+// simple, node-local test version of gmres
 template<typename P>
 P simple_gmres(fk::matrix<P> const &A, fk::vector<P> &x, fk::vector<P> const &b,
                fk::matrix<P> const &M, int const restart, int const max_iter,
