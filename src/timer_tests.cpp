8395e75355dfc00d8e5ce030968163c5e7bcbedb
Tyler McDaniel <bmcdan16@utk.edu>
2020-04-07 18:52:13 -0400

    ...

--- src/timer_tests.cpp
+++ src/timer_tests.cpp
@@ -100,7 +100,7 @@ TEST_CASE("test recorder")
     }
     double const gold_average = sum / times.size();
     REQUIRE(avg == gold_average);
-    relaxed_comparison(avg_wrap, gold_average, tolerance);
+    relaxed_fp_comparison(avg_wrap, gold_average, tolerance);
   }
 
   SECTION("min/max")
@@ -116,8 +116,8 @@ TEST_CASE("test recorder")
 
     REQUIRE(min == gold_min);
     REQUIRE(max == gold_max);
-    relaxed_comparison(min_wrap, gold_min, tolerance);
-    relaxed_comparison(max_wrap, gold_max, tolerance);
+    relaxed_fp_comparison(min_wrap, gold_min, tolerance);
+    relaxed_fp_comparison(max_wrap, gold_max, tolerance);
   }
 
   SECTION("med")
@@ -129,7 +129,7 @@ TEST_CASE("test recorder")
                                 ? (time_copy[mid] + time_copy[mid - 1]) / 2
                                 : time_copy[mid];
     REQUIRE(med == gold_med);
-    relaxed_comparison(med_wrap, gold_med, tolerance);
+    relaxed_fp_comparison(med_wrap, gold_med, tolerance);
   }
 
   SECTION("count")
