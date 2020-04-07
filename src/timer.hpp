9096aabb55c3bebd3d86d9882a25619df8f76d20
Tyler McDaniel <bmcdan16@utk.edu>
2020-04-07 18:28:25 -0400

    update timer to provide for tic/toc interface

--- src/timer.hpp
+++ src/timer.hpp
@@ -22,6 +22,7 @@ public:
           !std::is_same_v<decltype(f(std::forward<Args>(args)...)), void>,
           decltype(f(std::forward<Args>(args)...))>::type
   {
+    assert(!identifier.empty());
     auto const beg = std::chrono::high_resolution_clock::now();
     auto const ret = std::forward<F>(f)(std::forward<Args>(args)...);
     auto const end = std::chrono::high_resolution_clock::now();
@@ -38,6 +39,7 @@ public:
           std::is_same_v<decltype(f(std::forward<Args>(args)...)), void>,
           void>::type
   {
+    assert(!identifier.empty());
     auto const beg = std::chrono::high_resolution_clock::now();
     std::forward<F>(f)(std::forward<Args>(args)...);
     auto const end = std::chrono::high_resolution_clock::now();
@@ -47,26 +49,51 @@ public:
     insert(identifier, dur);
   }
 
+  std::string start(std::string const identifier)
+  {
+    assert(!identifier.empty());
+    assert(id_to_start_.count(identifier) == 0);
+    id_to_start_[identifier] = std::chrono::high_resolution_clock::now();
+    return identifier;
+  }
+
+  void stop(std::string const identifier)
+  {
+    assert(!identifier.empty());
+    assert(id_to_start_.count(identifier) == 1);
+    auto const beg = id_to_start_[identifier];
+    auto const end = std::chrono::high_resolution_clock::now();
+    auto const dur =
+        std::chrono::duration_cast<std::chrono::milliseconds>(end - beg)
+            .count();
+    id_to_start_.erase(identifier);
+    insert(identifier, dur);
+  }
+
   // get performance report for recorded functions
   std::string report();
 
   // get times for some key, mostly for testing for now
   std::vector<double> const &get_times(std::string const id)
   {
-    assert(id_to_times.count(id) == 1);
-    return id_to_times[id];
+    assert(id_to_times_.count(id) == 1);
+    return id_to_times_[id];
   }
 
 private:
   // little helper for creating a new list if no values exist for key
   void insert(std::string const &key, double const time)
   {
-    id_to_times.try_emplace(key, std::vector<double>());
-    id_to_times[key].push_back(time);
+    id_to_times_.try_emplace(key, std::vector<double>());
+    id_to_times_[key].push_back(time);
   }
 
   // stores function identifier -> list of operator()times recorded
-  std::map<std::string, std::vector<double>> id_to_times;
+  std::map<std::string, std::vector<double>> id_to_times_;
+
+  std::map<std::string,
+           std::chrono::time_point<std::chrono::high_resolution_clock>>
+      id_to_start_;
 };
 extern recorder record;
 } // namespace timer
