21ea51958f2f4ba46efa0c41babf18132f4550a2
Benjamin T. McDaniel <bmcdan16@utk.edu>
2020-04-30 13:56:48 -0400

    fix compiler warnings causing CI to fail

--- contrib/contrib.cmake
+++ contrib/contrib.cmake
@@ -206,7 +206,7 @@ if(NOT KRON_LIB)
     ExternalProject_Add (kronmult-ext
       UPDATE_COMMAND ""
       PREFIX ${CMAKE_SOURCE_DIR}/contrib/kronmult
-      URL https://github.com/project-asgard/kronmult/archive/v0.4.tar.gz
+      URL https://github.com/project-asgard/kronmult/archive/v0.4.1.tar.gz
       DOWNLOAD_NO_PROGRESS 1
       CMAKE_ARGS ${KRON_ARGS}
       BUILD_IN_SOURCE 1
---
+++ src/kronmult_tests.cpp
@@ -0,0 +1,7 @@
+#include "tests_general.hpp"
+
+#include "kronmult.hpp"
+
+TEMPLATE_TEST_CASE("placeholder", "[kronmult]", float, double) {
+  REQUIRE(true);
+}
