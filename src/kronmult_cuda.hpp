9ceaee2883bea2c60d0aeb0442216ba4459074fd
Tyler McDaniel <bmcdan16@utk.edu>
2020-05-20 10:29:17 -0400

    trim old

--- src/kronmult_cuda.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-template<typename P>
-void call_kronmult(int const n, P *x_ptrs[], P *output_ptrs[], P *work_ptrs[],
-                   P const *const operator_ptrs[], int const lda,
-                   int const num_krons, int const num_dims);
