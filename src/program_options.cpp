50bd3edf33e6f38f6a75b238effcc73cadc41a17
Steven Hahn <hahnse@ornl.gov>
2021-01-20 10:49:16 -0500

    ASFARD -> ASGARD

--- src/program_options.cpp
+++ src/program_options.cpp
@@ -176,7 +176,7 @@ parser::parser(int argc, char **argv)
     {
       solver_str = "direct";
     }
-#ifndef ASFARD_USE_SLATE
+#ifndef ASGARD_USE_SLATE
     if (solver_str == "slate")
     {
       std::cerr << "Invalid solver choice; ASGarD not built with SLATE option "
