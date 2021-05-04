2c9af25fe3dcfca68cc59c848d62f66836908a26
Steven Hahn <hahnse@ornl.gov>
2021-05-04 18:03:37 -0400

    use snprintf
    Signed-off-by: Steven Hahn <hahnse@ornl.gov>

--- src/tools.cpp
+++ src/tools.cpp
@@ -6,6 +6,7 @@
 #include <math.h>
 #include <numeric>
 #include <sstream>
+#include <cstdio>
 
 namespace tools
 {
@@ -13,7 +14,9 @@ std::string simple_timer::report()
 {
   std::ostringstream report;
   report << "\nperformance report, all times in ms...\n\n";
-
+  const char * fmt1 = " - avg: %f min: %f max: %f med: %f";
+  const char * fmt2 = " calls: %d \n";
+  std::cout << id_to_times_.size();
   for (auto [id, times] : id_to_times_)
   {
     auto const avg =
@@ -45,14 +48,21 @@ std::string simple_timer::report()
       }
       return std::string("");
     }();
-
-    report << std::fixed << std::setprecision(4) << id << " - avg: " << avg
-           << " min: " << min << " max: " << max << " med: " << med << avg_flops
-           << " calls: " << times.size() << '\n';
+    auto size1 = snprintf(NULL, 0, fmt1, (double)avg, (double)min, (double)max, (double)med);
+    auto size2 = snprintf(NULL, 0, fmt2, times.size());
+    std::string out1(size1+1, ' ');
+    std::string out2(size2+1, ' ');
+    snprintf(out1.data(), size1 + 1, fmt1, avg, min, max, med);
+    snprintf(out2.data(), size2 + 1, fmt2, times.size());
+    report << id << out1 << avg_flops << out2;
+    //std::cout << id << out1 << avg_flops << out2;
+    //report << id << " - avg: " << avg << " min: " << min << " max: " << max
+    //       << " med: " << med << avg_flops << " calls: " << times.size()
+    //       << '\n';
   }
+  //std::cout << "report:\n" << report.str();
   return report.str();
 }
-
 simple_timer timer;
 
 } // namespace tools
