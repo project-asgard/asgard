97fea9714131cbfbe6f30cd5536ba4283642b718
Steven Hahn <hahnse@ornl.gov>
2022-03-31 15:54:18 -0400

    createFlist member function
    Signed-off-by: Steven Hahn <hahnse@ornl.gov>

--- src/moment.cpp
+++ src/moment.cpp
@@ -1,8 +1,38 @@
 #include "moment.hpp"
+#include "transformations.hpp"
 
 template<typename P>
-moment<P>::moment(std::vector<vector_func<P>> md_func) : md_func_(md_func)
+moment<P>::moment(std::vector<vector_func<P>> md_funcs_)
+    : md_funcs(std::move(md_funcs_))
 {}
+
+// Creates the coefficients of the moment vector on each domain.
+// No mass matrix inversion is done.
+template<typename P>
+void moment<P>::createFlist(PDE<P> const &pde, options const &opts)
+{
+  std::size_t num_md_funcs = this->md_funcs.size();
+
+  auto const &dims     = pde.get_dimensions();
+  std::size_t num_dims = dims.size();
+
+  this->fList.resize(num_md_funcs);
+  for (auto &elem : this->fList)
+  {
+    elem.resize(num_dims);
+  }
+
+  basis::wavelet_transform<P, resource::host> const transformer(opts, pde);
+
+  for (std::size_t s = 0; s < num_md_funcs; ++s)
+  {
+    auto const &md_func = this->md_funcs[s];
+    for (std::size_t d = 0; d < num_dims; ++d)
+    {
+      fList[s][d] = forward_transform<P>(dims[d], md_func, transformer);
+    }
+  }
+}
 
 template class moment<float>;
 template class moment<double>;
--- src/moment.hpp
+++ src/moment.hpp
@@ -1,18 +1,19 @@
 #pragma once
 
 #include "pde/pde_base.hpp"
+#include "program_options.hpp"
 #include <vector>
 
 template<typename P>
 class moment
 {
 public:
-  moment(std::vector<vector_func<P>> md_func);
-  // createFlist(std::vector<std::unique_ptr<PDE<P>>> pde);
+  moment(std::vector<vector_func<P>> md_funcs_);
+  void createFlist(PDE<P> const &pde, options const &opts);
   // createMomentVector()
 private:
-  std::vector<vector_func<P>> md_func_;
-  // fList;
+  std::vector<vector_func<P>> md_funcs;
+  std::vector<std::vector<fk::vector<P>>> fList;
   // vector;
   // moment_fval_integral;
   // moment_analytic_integral;
