6c015b0e64c7647d4f032450b9c74fa99e9268fd
Miroslav Stoyanov <stoyanovmk@ornl.gov>
2022-09-23 18:02:13 -0400

    better permutations, generalized and without recursion

--- src/permutations.cpp
+++ src/permutations.cpp
@@ -10,220 +10,6 @@
 
 namespace permutations
 {
-//
-// Permutation enumerators
-//
-
-// Given the number of dimensions and a limit, count the number of n-tuples
-// (where n == 'num_dims') whose non-negative elements' sum == 'limit'
-int count_equal(int const num_dims, int const limit)
-{
-  expect(num_dims > 0);
-  expect(limit >= 0);
-  if (num_dims == 1)
-  {
-    return 1;
-  }
-
-  if (num_dims == 2)
-  {
-    return limit + 1;
-  }
-
-  int count = 0;
-  for (auto i = 0; i <= limit; ++i)
-  {
-    count += count_equal(num_dims - 1, i);
-  }
-
-  return count;
-}
-
-// Given the number of dimensions and a limit, count the number of n-tuples
-// (where n == 'num_dims') whose non-negative elements' sum <= 'limit'
-// this version handles the non-uniform level case
-int count_equal_multi(fk::vector<int> const &levels, int const num_dims,
-                      int const limit)
-{
-  expect(num_dims > 0);
-  expect(levels.size() == num_dims);
-  expect(limit >= 0);
-
-  if (num_dims == 1)
-  {
-    if (levels(0) >= limit)
-    {
-      return 1;
-    }
-    return 0;
-  }
-
-  auto count = 0;
-  for (auto i = 0; i <= std::min(levels(num_dims - 1), limit); ++i)
-  {
-    auto const dim_limit = limit - i;
-    count += count_equal_multi(levels.extract(0, num_dims - 2), num_dims - 1,
-                               dim_limit);
-  }
-
-  return count;
-}
-
-// Given the number of dimensions and a limit, count the number of n-tuples
-// (where n == 'num_dims') whose non-negative elements' sum <= 'limit'
-int count_lequal(int const num_dims, int const limit)
-{
-  expect(num_dims > 0);
-  expect(limit >= 0);
-
-  int count = 0;
-  for (auto i = 0; i <= limit; ++i)
-  {
-    count += count_equal(num_dims, i);
-  }
-  return count;
-}
-
-// Given the number of dimensions and a limit, count the number of n-tuples
-// (where n == 'num_dims') whose non-negative elements' sum <= 'limit'
-// this version handles the non-uniform level case
-int count_lequal_multi(fk::vector<int> const &levels, int num_dims, int limit)
-{
-  expect(num_dims > 0);
-  expect(levels.size() == num_dims);
-  expect(limit >= 0);
-
-  auto count = 0;
-  for (auto i = 0; i <= limit; ++i)
-  {
-    count += count_equal_multi(levels, num_dims, i);
-  }
-
-  return count;
-}
-
-// Given the number of dimensions and a limit, count the number of n-tuples
-// (where n == 'num_dims') whose non-negative max element <= 'limit' (for full
-// grid only)
-int count_max(int const num_dims, int const limit)
-{
-  expect(num_dims > 0);
-  expect(limit >= 0);
-
-  return static_cast<int>(std::pow(limit + 1, num_dims));
-}
-
-//
-// Permutations builders
-//
-
-// Given the number of dimensions and a limit, produce n-tuples (where n ==
-// 'num_dims') whose elements' are non-negative and their sum == 'limit'. Each
-// tuple becomes a row of the output matrix
-fk::matrix<int>
-get_equal(int const num_dims, int const limit, bool const order_by_n)
-{
-  expect(num_dims > 0);
-  expect(limit >= 0);
-
-  int const num_tuples = count_equal(num_dims, limit);
-  fk::matrix<int> result(num_tuples, num_dims);
-
-  if (num_dims == 1)
-  {
-    return fk::matrix<int>{{limit}};
-  }
-
-  int counter = 0;
-  for (auto i = 0; i <= limit; ++i)
-  {
-    int partial_sum;
-    int difference;
-
-    if (order_by_n)
-    {
-      partial_sum = i;
-      difference  = limit - i;
-    }
-    else
-    {
-      partial_sum = limit - i;
-      difference  = i;
-    }
-
-    // build set of num_dims-1 sized tuples which sum to partial_sum,
-    // then append a column with difference to make a num_dims-tuple.
-    int const rows = count_equal(num_dims - 1, partial_sum);
-    fk::matrix<int> partial_result(rows, num_dims);
-    partial_result.set_submatrix(
-        0, 0, get_equal(num_dims - 1, partial_sum, order_by_n));
-    fk::vector<int> last_col = std::vector<int>(rows, difference);
-    partial_result.update_col(num_dims - 1, last_col);
-    result.set_submatrix(counter, 0, partial_result);
-
-    counter += rows;
-  }
-  return result;
-}
-
-// Given the number of dimensions and a limit, produce n-tuples (where n ==
-// 'num_dims') whose elements' are non-negative and their sum == 'limit'. Each
-// tuple becomes a row of the output matrix
-// this version handles the non-uniform level case via a vector "levels"
-// argument
-fk::matrix<int> get_equal_multi(fk::vector<int> const &levels,
-                                int const num_dims, int const limit,
-                                bool const last_index_decreasing)
-{
-  expect(num_dims > 0);
-  expect(levels.size() == num_dims);
-  expect(limit >= 0);
-
-  if (num_dims == 1)
-  {
-    if (levels(0) >= limit)
-    {
-      return fk::matrix<int>{{limit}};
-    }
-    else
-    {
-      return fk::matrix<int>();
-    }
-  }
-
-  auto const num_tuples = count_equal_multi(levels, num_dims, limit);
-  fk::matrix<int> result(num_tuples, num_dims);
-  auto row_counter = 0;
-  auto const stop  = std::min(levels(num_dims - 1), limit);
-
-  for (auto i = 0; i <= stop; ++i)
-  {
-    auto const last_elem     = last_index_decreasing ? stop - i : i;
-    auto const partial_limit = limit - last_elem;
-    auto const partial_size = count_equal_multi(levels.extract(0, num_dims - 2),
-                                                num_dims - 1, partial_limit);
-
-    if (partial_size < 1)
-    {
-      continue;
-    }
-
-    fk::matrix<int, mem_type::view> partial_result(
-        result, row_counter, row_counter + partial_size - 1, 0, num_dims - 2);
-    partial_result =
-        get_equal_multi(levels.extract(0, num_dims - 2), num_dims - 1,
-                        partial_limit, last_index_decreasing);
-
-    fk::matrix<int, mem_type::view> partial_last_col(
-        result, row_counter, row_counter + partial_size - 1, num_dims - 1,
-        num_dims - 1);
-    partial_last_col = fk::vector(std::vector<int>(partial_size, last_elem));
-    row_counter += partial_size;
-  }
-
-  return result;
-}
-
 // Given the number of dimensions and a limit, produce n-tuples (n ==
 // 'num_dims') whose elements are non-negative and their sum <= 'limit'. Each
 // tuple becomes a row of the output matrix
@@ -233,45 +19,13 @@ get_lequal(int const num_dims, int const limit, bool const order_by_n)
   expect(num_dims > 0);
   expect(limit >= 0);
 
-  int const num_tuples = count_lequal(num_dims, limit);
-  fk::matrix<int> result(num_tuples, num_dims);
-
-  if (order_by_n)
-  {
-    int counter = 0;
-    for (auto i = 0; i <= limit; ++i)
-    {
-      int const rows = count_equal(num_dims, i);
-      result.set_submatrix(counter, 0, get_equal(num_dims, i, false));
-      counter += rows;
-    }
-    return result;
-  }
-
-  // the recursive base case
-  if (num_dims == 1)
-  {
-    std::vector<int> entries(limit + 1);
-    std::iota(begin(entries), end(entries), 0);
-    result.update_col(0, entries);
-    return result;
-  }
-
-  int counter = 0;
-  for (auto i = 0; i <= limit; ++i)
-  {
-    int const rows = count_lequal(num_dims - 1, limit - i);
-    fk::matrix<int> partial_result(rows, num_dims);
-    partial_result.set_submatrix(
-        0, 0, get_lequal(num_dims - 1, limit - i, order_by_n));
-    fk::vector<int> last_col = std::vector<int>(rows, i);
-    partial_result.update_col(num_dims - 1, last_col);
-    result.set_submatrix(counter, 0, partial_result);
-
-    counter += rows;
-  }
-
-  return result;
+  return select_indexex(num_dims, order_by_n, false,
+                        [&](std::vector<int> const &index) -> bool {
+                          int level = index[0];
+                          for (size_t i = 1; i < index.size(); i++)
+                            level += index[i];
+                          return (level <= limit);
+                        });
 }
 
 // Given the number of dimensions and a limit, produce n-tuples (n ==
@@ -286,25 +40,17 @@ fk::matrix<int> get_lequal_multi(fk::vector<int> const &levels,
   expect(levels.size() == num_dims);
   expect(limit >= 0);
 
-  auto const num_tuples = count_lequal_multi(levels, num_dims, limit);
-  fk::matrix<int> result(num_tuples, num_dims);
-
-  auto row_counter = 0;
-  for (auto i = 0; i <= limit; ++i)
-  {
-    auto const partial_limit = increasing_sum_order ? i : limit - i;
-    auto const partial_size =
-        count_equal_multi(levels, num_dims, partial_limit);
-    if (partial_size > 0)
-    {
-      fk::matrix<int, mem_type::view> partial_result(
-          result, row_counter, row_counter + partial_size - 1, 0, num_dims - 1);
-      partial_result = get_equal_multi(levels, num_dims, partial_limit, false);
-      row_counter += partial_size;
-    }
-  }
-
-  return result;
+  return select_indexex(num_dims, true, not increasing_sum_order,
+                        [&](std::vector<int> const &index) -> bool {
+                          int level = 0;
+                          for (size_t i = 0; i < index.size(); i++)
+                          {
+                            if (index[i] > levels(i))
+                              return false;
+                            level += index[i];
+                          }
+                          return (level <= limit);
+                        });
 }
 
 // Given the number of dimensions and a limit, produce n-tuples (n ==
@@ -316,42 +62,8 @@ get_max(int const num_dims, int const limit, bool const last_index_decreasing)
   expect(num_dims > 0);
   expect(limit >= 0);
 
-  int const num_tuples = count_max(num_dims, limit);
-  fk::matrix<int> result(num_tuples, num_dims);
-
-  if (num_dims == 1)
-  {
-    std::vector<int> entries(limit + 1);
-    std::iota(begin(entries), end(entries), 0);
-    if (last_index_decreasing)
-    {
-      std::reverse(begin(entries), end(entries));
-    }
-    result.update_col(0, entries);
-    return result;
-  }
-
-  // recursively build the lower dim tuples
-  fk::matrix<int> lower_dims =
-      get_max(num_dims - 1, limit, last_index_decreasing);
-  int const m = lower_dims.nrows();
-
-  for (auto i = 0; i <= limit; ++i)
-  {
-    int const row_position = i * m;
-    int const rows         = m;
-    int const last_entry   = last_index_decreasing ? (limit - i) : i;
-
-    fk::matrix<int> partial_result(rows, num_dims);
-    fk::matrix<int> lower_dims_i =
-        lower_dims.extract_submatrix(0, 0, m, num_dims - 1);
-    partial_result.set_submatrix(0, 0, lower_dims_i);
-    fk::vector<int> last_col = std::vector<int>(rows, last_entry);
-    partial_result.update_col(num_dims - 1, last_col);
-    result.set_submatrix(row_position, 0, partial_result);
-  }
-
-  return result;
+  return get_max_multi(std::vector<int>(num_dims, limit), num_dims,
+                       last_index_decreasing);
 }
 
 // Given the number of dimensions and a limit, produce n-tuples (n ==
@@ -364,38 +76,34 @@ fk::matrix<int> get_max_multi(fk::vector<int> const &levels, int const num_dims,
   expect(num_dims > 0);
   expect(levels.size() > 0);
 
-  auto const limit = levels(num_dims - 1);
+  int num_indexes = levels(0) + 1;
+  for (int j = 1; j < levels.size(); j++)
+    num_indexes *= (levels(j) + 1);
 
-  if (num_dims == 1)
-  {
-    std::vector<int> entries(limit + 1);
-    std::iota(begin(entries), end(entries), 0);
-    if (last_index_decreasing)
+  fk::matrix<int> result(num_indexes, num_dims);
+
+  auto load_multiindex = [&](int row, int i) -> void {
+    int t = i;
+    for (int j = 0; j < num_dims; j++)
     {
-      std::reverse(begin(entries), end(entries));
+      result(row, j) = t % (levels(j) + 1);
+      t /= (levels(j) + 1);
     }
-    return fk::matrix<int>(limit + 1, 1).update_col(0, entries);
-  }
+  };
 
-  auto const lower_perm =
-      get_max_multi(levels, num_dims - 1, last_index_decreasing);
-  fk::matrix<int> result(lower_perm.nrows() * (limit + 1), num_dims);
-
-  for (auto i = 0; i <= limit; ++i)
+  if (last_index_decreasing)
   {
-    auto const row_pos  = i * lower_perm.nrows();
-    auto const row_stop = row_pos + lower_perm.nrows() - 1;
-
-    fk::matrix<int, mem_type::view> partial_result(result, row_pos, row_stop, 0,
-                                                   num_dims - 2);
-    partial_result = fk::matrix<int, mem_type::const_view>(
-        lower_perm, 0, lower_perm.nrows() - 1, 0, num_dims - 2);
-
-    fk::vector<int, mem_type::view> last_col(result, num_dims - 1, row_pos,
-                                             row_stop);
-    auto const last_col_val = last_index_decreasing ? limit - i : i;
-    last_col =
-        fk::vector<int>(std::vector<int>(row_stop - row_pos + 1, last_col_val));
+    for (int i = num_indexes - 1; i >= 0; i--)
+    {
+      load_multiindex(num_indexes - i - 1, i);
+    }
+  }
+  else
+  {
+    for (int i = 0; i < num_indexes; i++)
+    {
+      load_multiindex(i, i);
+    }
   }
 
   return result;
--- src/permutations.hpp
+++ src/permutations.hpp
@@ -8,41 +8,269 @@
 // construction of the element table
 // -----------------------------------------------------------------------------
 
+/*!
+ * \defgroup permuts Permutations and index selection
+ *
+ * Collection of algorithms for generating multi-indexes
+ * with different order and selection criteria.
+ */
+
 namespace permutations
 {
-// Permutations counters
+/*!
+ * \ingroup permuts
+ * \brief Generates the list of multi-indexes that satisfy a general selection
+ * criteria.
+ *
+ * A helper method to select_indexex(), it generates a list of vectors,
+ * where the first index is in entries 0 to `num_dims-1`
+ * and all follow on indexes are appended one after the other;
+ * the order of the indexes is lexicographical ascending starting with the first
+ * dimension.
+ */
+inline std::vector<int> generate_lower_index_set(
+    size_t num_dims, std::function<bool(std::vector<int> const &index)> inside)
+{
+  size_t c   = 0;
+  bool is_in = true;
+  std::vector<int> root(num_dims, 0);
+  std::vector<int> indexes;
+  while (is_in || (c < num_dims))
+  {
+    if (is_in)
+    {
+      indexes.insert(indexes.end(), root.begin(), root.end());
+      c = 0;
+      root[c]++;
+    }
+    else
+    {
+      std::fill(root.begin(), root.begin() + c + 1, 0);
+      c++;
+      if (c == num_dims)
+        break;
+      root[c]++;
+    }
+    is_in = inside(root);
+  }
+  return indexes;
+}
+/*!
+ * \ingroup permuts
+ * \brief Generates the list of multi-indexes that satisfy a general selection
+ * criteria.
+ *
+ * Identical to generate_lower_index_set(), but returns the list sorted in
+ * levels. The first vector in the result will always be the zeroth vector, the
+ * second vector will contain the selected set of indexes from among the indexes
+ * that have one entry equal to 1, the L-th vector contains the selected indexes
+ * with sum of the entries equal to L.
+ */
+inline std::vector<std::vector<int>> generate_lower_index_level_sets(
+    size_t num_dims, std::function<bool(std::vector<int> const &index)> inside)
+{
+  size_t c = 0, level = 0;
+  bool is_in = true;
+  std::vector<int> root(num_dims, 0);
+  std::vector<std::vector<int>> indexes(1);
+  while (is_in || (c < num_dims))
+  {
+    if (is_in)
+    {
+      if (level == indexes.size())
+      {
+        indexes.push_back(std::vector<int>(root));
+      }
+      else
+      {
+        indexes[level].insert(indexes[level].end(), root.begin(), root.end());
+      }
+      c = 0;
+      root[c]++;
+      level++;
+    }
+    else
+    {
+      std::fill(root.begin(), root.begin() + c + 1, 0);
+      c++;
+      if (c == num_dims)
+        break;
+      root[c]++;
+      level = root[0];
+      for (size_t i = 1; i < root.size(); i++)
+        level += root[i];
+    }
+    is_in = inside(root);
+  }
+  return indexes;
+}
+/*!
+ * \ingroup permuts
+ * \brief Converts the output of generate_lower_index_set() into
+ * fk::matrix<int>.
+ *
+ * \param num_dims must be the same as in the call to generate_lower_index_set()
+ * \param indexes must be the output of generate_lower_index_set()
+ *
+ * \returns an fk::matrix<int> containing all the indexes.
+ */
+inline fk::matrix<int>
+transp_data(int const num_dims, std::vector<int> const &indexes)
+{
+  int const nrows = indexes.size() / num_dims;
+  fk::matrix<int> result(nrows, num_dims);
+  for (int i = 0; i < nrows; i++)
+  {
+    for (int j = 0; j < num_dims; j++)
+    {
+      result(i, j) = indexes[i * num_dims + j];
+    }
+  }
+  return result;
+}
+/*!
+ * \ingroup permuts
+ * \brief Converts the output of generate_lower_index_level_sets() into
+ * fk::matrix<int>.
+ *
+ * See transp_data().
+ *
+ * \b levels_in_reverse will switch the order of levels, so the zeroth-vector
+ * will be last.
+ */
+inline fk::matrix<int> transp_data(int const num_dims, bool levels_in_reverse,
+                                   std::vector<std::vector<int>> const &indexes)
+{
+  int nrows = 0;
+  for (auto const &ivec : indexes)
+    nrows += ivec.size() / num_dims;       // count all indexes
+  fk::matrix<int> result(nrows, num_dims); // allocate the matrix
 
-int count_equal(int const num_dims, int const limit);
+  if (levels_in_reverse)
+  {
+    int count = nrows; // count the current set of rows
+    for (auto const &ivec : indexes)
+    {
+      nrows = ivec.size() / num_dims;
+      count -= nrows;
+      for (int i = 0; i < nrows; i++)
+      {
+        for (int j = 0; j < num_dims; j++)
+        {
+          result(i + count, j) = ivec[i * num_dims + j];
+        }
+      }
+    }
+  }
+  else
+  {
+    int count = 0; // count the current set of rows
+    for (auto const &ivec : indexes)
+    {
+      nrows = ivec.size() / num_dims;
+      for (int i = 0; i < nrows; i++)
+      {
+        for (int j = 0; j < num_dims; j++)
+        {
+          result(i + count, j) = ivec[i * num_dims + j];
+        }
+      }
+      count += nrows;
+    }
+  }
+  return result;
+}
+/*!
+ * \ingroup permuts
+ * \brief Generates a set of multi-indexes and returns them into a matrix with
+ * the appropriate sorting order.
+ *
+ * \param num_dims is the number of dimensions or the size of the multi-index
+ * tuple, must be at least 1 \param order_by_n indicates whether the indexes
+ * should be sorted by the level, if order_by_n is true, and the sum of entries
+ * of i is less than the sum entries of j, then i will be on a row before j
+ * \param levels_in_reverse indicates whether the zeroth vector should be first
+ * (false) or last (true) in the ordering, used only when order_by_n is true.
+ * \param inside is a callable method that returns true whenever the provided
+ * index satisfies the criteria
+ * - the input to \b inside will always be a vector of size num_dims
+ * - the criteria given by inside must describe a lower completer set
+ *   a set \b A is lower complete whenever \f$ j \leq i \f$ and \f$ i \in A \f$
+ * guarantees \f$ j \in A \f$ (an alternative definition is that a lower
+ * complete set is the union of dense/full sets)
+ *
+ * \returns fk::matrix<int> containing the selected multi-indexes
+ */
+inline fk::matrix<int>
+select_indexex(int const num_dims, bool order_by_n, bool levels_in_reverse,
+               std::function<bool(std::vector<int> const &index)> inside)
+{
+  if (order_by_n)
+  {
+    return transp_data(num_dims, levels_in_reverse,
+                       generate_lower_index_level_sets(num_dims, inside));
+  }
+  else
+  {
+    return transp_data(num_dims, generate_lower_index_set(num_dims, inside));
+  }
+}
 
-int count_equal_multi(fk::vector<int> const &levels, int const num_dims,
-                      int const limit);
-
-int count_lequal(int const num_dims, int const limit);
-
-int count_lequal_multi(fk::vector<int> const &levels, int const num_dims,
-                       int const limit);
-
-int count_max(int const num_dims, int const limit);
-
-// Permutations builders
-
-fk::matrix<int>
-get_equal(int const num_dims, int const limit, bool const order_by_n);
-
-fk::matrix<int> get_equal_multi(fk::vector<int> const &levels,
-                                int const num_dims, int const limit,
-                                bool const last_index_decreasing);
-
+/*!
+ * \ingroup permuts
+ * \brief Select multi-indexes with sum of the entries below a limit.
+ *
+ * \param num_dims is dimensions of the multi-indexes
+ * \param limit is the cut-off criteria, selected indexes will have
+ *        vector one norm (or sum of entries) less or equal to this limit
+ * \param order_by_n indicates the order of the indexes, if set to true the
+ * indexes will be grouped by matching the sum of indexes
+ *
+ * \returns matrix with num_dims columns where each row is a multi-index
+ */
 fk::matrix<int>
 get_lequal(int const num_dims, int const limit, bool const order_by_n);
-
+/*!
+ * \ingroup permuts
+ * \brief Select similar to get_lequal() but also applies limits in different
+ * directions.
+ *
+ * Uses the same selection as permutations::get_lequal() but imposes additional
+ * constraint in each direction.
+ * Every index entry will be no greater than the corresponding entry in \b
+ * levels, naturally, expecting levels.size() == num_dims and levels[j] >= 0.
+ */
 fk::matrix<int> get_lequal_multi(fk::vector<int> const &levels,
                                  int const num_dims, int const limit,
                                  bool const increasing_sum_order);
 
+/*!
+ * \ingroup permuts
+ * \brief Generates multi-indexes for a full/dense grid.
+ *
+ * \param num_dims is dimensions of the multi-indexes
+ * \param limit is the largest index entry
+ * \param last_index_decreasing indicates whether to sort the indexes
+ *        in ascending (false) or descending order
+ *
+ * \returns matrix with num_dims columns where each row is a multi-index
+ */
 fk::matrix<int>
 get_max(int const num_dims, int const limit, bool const last_index_decreasing);
 
+/*!
+ * \ingroup permuts
+ * \brief Generates non-uniform (anisotropic) multi-indexes for a full/dense
+ * grid.
+ *
+ * \param levels indicates the largest index in each direction,
+ *        expecting levels.size() == num_dims and levels[j] >= 0
+ * \param num_dims is dimensions of the multi-indexes
+ * \param last_index_decreasing indicates whether to sort the indexes
+ *        in ascending (false) or descending order
+ *
+ * \returns matrix with num_dims columns where each row is a multi-index
+ */
 fk::matrix<int> get_max_multi(fk::vector<int> const &levels, int const num_dims,
                               bool const last_index_decreasing);
 
