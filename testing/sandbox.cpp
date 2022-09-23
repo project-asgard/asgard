da12d868605d8be8877760b68df93541f4c0932d
Miroslav Stoyanov <30537612+mkstoyanov@users.noreply.github.com>
2022-09-23 16:29:25 -0400

    Add sandbox file (#436)
    * removed unused cmake variable
    
    * useful file

---
+++ testing/sandbox.cpp
@@ -0,0 +1,36 @@
+#include "batch.hpp"
+
+#include "build_info.hpp"
+#include "coefficients.hpp"
+#include "distribution.hpp"
+#include "elements.hpp"
+#include "tools.hpp"
+
+#ifdef ASGARD_IO_HIGHFIVE
+#include "io.hpp"
+#endif
+
+#ifdef ASGARD_USE_MPI
+#include <mpi.h>
+#endif
+
+#include "pde.hpp"
+#include "program_options.hpp"
+#include "tensors.hpp"
+#include "time_advance.hpp"
+#include "transformations.hpp"
+#include <numeric>
+
+#ifdef ASGARD_USE_DOUBLE_PREC
+using prec = double;
+#else
+using prec = float;
+#endif
+
+int main(int, char**)
+{
+  // keep this file clean for each PR
+  // allows someone to easily come here, dump code and start playing
+  // this is good for prototyping and quick-testing features/behavior
+  return 0;
+}
