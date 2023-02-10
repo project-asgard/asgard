f87f2af8f8c5521a23ce34001f50586392b80f5b
Steven Hahn <hahnse@ornl.gov>
2023-02-10 16:58:12 -0500

    Add location of 1D slices to figure
    Signed-off-by: Steven Hahn <hahnse@ornl.gov>

--- scripts/matlab/plot_fval.m
+++ scripts/matlab/plot_fval.m
@@ -48,7 +48,8 @@ if num_dims==2
     y = nodes{2};
     ax1 = subplot(2,2,1);
     plot(x,f_slice,'-o');
-    title('1D slice (vertical)');
+    legend_info = sprintf('1D slice (vertical) x = %5.4f',y(sy));
+    title(legend_info);
     
     %%
     % Overplot analytic solution
@@ -70,7 +71,8 @@ if num_dims==2
     y = nodes{2};
     ax1 = subplot(2,2,2);
     plot(y,f_slice,'-o');
-    title('1D slice (horizontal)');
+    legend_info = sprintf('1D slice (horizontal) y = %5.4f',x(sx));
+    title(legend_info);
     
     if ~isempty(solutions)
         f_slice_analytic = f_nD_analytic(:,sx);
