0a0da62a8c26141121c879b4c464924839e8df7c
Miroslav Stoyanov <stoyanovmk@ornl.gov>
2023-07-27 14:44:05 -0400

    another issue

--- src/adapt.cpp
+++ src/adapt.cpp
@@ -382,15 +382,12 @@ fk::vector<P> distributed_grid<P>::remove_elements(
     node_out() << "coarsened below number of ranks - can't handle this case yet"
                << '\n';
   }
-  auto const new_plan = get_plan(get_num_ranks(), table_);
+  auto new_plan       = get_plan(get_num_ranks(), table_);
   auto const remapper = remap_for_delete(all_remove_indices, table_.size());
-  auto const y        = redistribute_vector(x, plan_, new_plan, remapper);
+  fk::vector<P> y     = redistribute_vector(x, plan_, new_plan, remapper);
 
-  // Clang and libc++ raise a const error if new_plan is directly assigned to
-  // plan_ since it tries to overwrite the elements inplace. Instead clear the
-  // elements of plan first then inser the elements from the new_plan.
-  plan_.clear();
-  plan_.insert(new_plan.cbegin(), new_plan.cend());
+  plan_ = std::move(new_plan);
+
   return y;
 }
 
