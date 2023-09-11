04f64f2b82c0672793e22c24846a4a28be9e82e4
Coleman Kendrick <6309092+ckendrick@users.noreply.github.com>
2023-09-11 09:26:03 -0400

    Add relaxation 1x3v PDE (#629)
    * Add relaxation 1x3v PDE
    
    * Update initial condition for x
    
    * Add triple maxwellian test case
    
    * Temporary fix to relaxation 1x3v to work on new kronmult
    
    * Remove old test cases and fix warnings

--- src/program_options.hpp
+++ src/program_options.hpp
@@ -52,6 +52,7 @@ enum class PDE_opts
   vlasov_two_stream,
   relaxation_1x1v,
   relaxation_1x2v,
+  relaxation_1x3v,
   collisional_landau
   // FIXME will need to add the user supplied PDE choice
 };
@@ -176,6 +177,11 @@ static pde_map_t const pde_mapping = {
                     "+ div_{v2} v_2 f + d_{v2} -u_2 f + d_{v1}(th q), q = "
                     "d_{v1} f + d_{v2}(th q), q = d_{v2} f",
                     PDE_opts::relaxation_1x2v)},
+    {"relaxation_1x3v",
+     PDE_descriptor("Relaxation 1x3v. df/dt == div_{v1} v_1 f + d_{v1} -u_1 f "
+                    "+ div_{v2} v_2 f + d_{v2} -u_2 f + d_{v1}(th q), q = "
+                    "d_{v1} f + d_{v2}(th q), q = d_{v2} f",
+                    PDE_opts::relaxation_1x3v)},
     {"landau", PDE_descriptor("Collisional Landau. df/dt == -v*grad_x f "
                               "-E*grad_v f + div_v((v-u)f + theta*grad_v f)",
                               PDE_opts::collisional_landau)}};
