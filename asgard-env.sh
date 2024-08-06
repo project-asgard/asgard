699bac6b876a375064165edac8c5656896de145d
Miroslav Stoyanov <stoyanovmk@ornl.gov>
2024-08-06 15:00:26 -0400

    fix typo in formatting

--- asgard-env.sh
+++ asgard-env.sh
@@ -2,7 +2,7 @@
 
 # file to source that will load the ASGarD environment
 
-export PATH=$PATH:"@CMAKE_INSTALL_PREFIX@"/bin/
+export PATH=$PATH:"@CMAKE_INSTALL_PREFIX@/bin/"
 
 if [[ "@ASGARD_USE_PYTHON@" == "ON" ]]; then
     export PYTHONPATH=$PYTHONPATH:"@_asgard_python_path@"
