1d3040104c4f9261dc225ee9434519e3c1848fff
Miroslav Stoyanov <mkstoyanov@gmail.com>
2024-11-22 20:41:53 -0500

    trying

--- contrib/asgard_io.cmake
+++ contrib/asgard_io.cmake
@@ -8,7 +8,7 @@
 # right now, we only access this through the HighFive wrapper lib
 ###############################################################################
 function (get_hdf5)
-  add_library (asgard_hdf5 INTERFACE)
+  add_library (asgard_hdf5 INTERFACE IMPORTED)
 
   if (NOT ASGARD_BUILD_HDF5)
     # search for hdf5 under user-supplied path(s)
@@ -77,10 +77,14 @@ function (get_hdf5)
 
     # either it was already here, or we just built it here
     set (hdf5_include ${__asgard_h5_install_prefix}/include)
-    set (hdf5_lib "${__asgard_h5_install_prefix}/lib/${CMAKE_SHARED_LIBRARY_PREFIX}hdf5${CMAKE_SHARED_LIBRARY_SUFFIX}")
+    #set (hdf5_lib "${__asgard_h5_install_prefix}/lib/${CMAKE_SHARED_LIBRARY_PREFIX}hdf5${CMAKE_SHARED_LIBRARY_SUFFIX}")
+    #set (hdf5_lib "-lhdf5")
+
+    set_target_properties(asgard_hdf5 PROPERTIES IMPORTED_NO_SONAME ON)
 
     target_include_directories (asgard_hdf5 INTERFACE $<BUILD_INTERFACE:${hdf5_include}>)
-    target_link_libraries (asgard_hdf5 INTERFACE $<BUILD_INTERFACE:${hdf5_lib}>)
+    target_link_directories (asgard_hdf5 INTERFACE $<BUILD_INTERFACE:${__asgard_h5_install_prefix}/lib/>)
+    target_link_libraries (asgard_hdf5 INTERFACE $<BUILD_INTERFACE:-lhdf5>)
   endif ()
 
 endfunction()
