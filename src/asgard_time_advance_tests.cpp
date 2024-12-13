675f6af9a08546232be8abeaf59c54f0c739d078
Miroslav Stoyanov <mkstoyanov@gmail.com>
2024-12-13 11:52:44 -0500

    disable bad test under single-precision

--- src/asgard_time_advance_tests.cpp
+++ src/asgard_time_advance_tests.cpp
@@ -130,18 +130,22 @@ TEST_CASE("adaptive time advance")
 
   SECTION("diffusion 2 implicit")
   {
-    auto const tol_factor = get_tolerance<default_precision>(1000);
-
-    auto const gold_base =
-        time_advance_base_dir / "diffusion2_ad_implicit_sg_l3_d4_t";
-
-    auto opts = make_opts("-p diffusion_2 -d 3 -l 3 -n 5 -s impl -a 0.05 -sv direct");
-
-    // temporarily disable test for MPI due to table elements < num ranks
-    if (get_num_ranks() == 1)
+    // the condition number for the difusion PDE is large, use double-precision only
+    if (std::is_same_v<default_precision, double>)
     {
-      time_advance_test(opts, gold_base, tol_factor);
+      auto const tol_factor = get_tolerance<default_precision>(1000);
+
+      auto const gold_base =
+          time_advance_base_dir / "diffusion2_ad_implicit_sg_l3_d4_t";
+
+      auto opts = make_opts("-p diffusion_2 -d 3 -l 3 -n 5 -s impl -a 0.05 -sv direct");
+
+      // temporarily disable test for MPI due to table elements < num ranks
+      if (get_num_ranks() == 1)
+        time_advance_test(opts, gold_base, tol_factor);
     }
+    else
+      REQUIRE(true);
   }
   SECTION("diffusion 2 explicit")
   {
