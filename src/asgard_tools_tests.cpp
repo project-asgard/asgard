97d01a563f93174023c3470834b0f27730a36c83
Miroslav Stoyanov <stoyanovmk@ornl.gov>
2025-04-11 17:49:34 -0400

    tool testing

--- src/asgard_tools_tests.cpp
+++ src/asgard_tools_tests.cpp
@@ -1,13 +1,51 @@
-#include "asgard_tools.hpp"
+#include "asgard_test_macros.hpp"
 
-#include "tests_general.hpp"
-
-#include <thread>
+#include <thread> // needed for "sleep"
 
 using namespace asgard;
 
-TEST_CASE("test timer", "[timing test]")
+void test_indexof()
 {
+  current_test name_("for (auto i : indexof(vec))");
+
+  std::vector<double> x(10);
+  std::vector<int64_t> r;
+  r.reserve(x.size());
+
+  for (auto i : indexof(x))
+  {
+    static_assert(std::is_same_v<decltype(i), int64_t>);
+    r.push_back(i);
+  }
+
+  tassert(r.size() == x.size());
+  for (int64_t i = 0; i < 10; i++)
+    tassert(r[i] == i);
+
+  std::vector<int> ir;
+  ir.reserve(8);
+
+  for (auto i : indexof<int>(1, 6))
+  {
+    static_assert(std::is_same_v<decltype(i), int>);
+    ir.push_back(i);
+  }
+
+  for (int i = 1; i < 6; i++)
+    tassert(ir[i - 1] == i);
+
+  size_t s = 0;
+  for (auto i : indexof<size_t>(x.size()))
+  {
+    static_assert(std::is_same_v<decltype(i), size_t>);
+    s += i;
+  }
+  tassert(s == 45);
+}
+
+void test_timer()
+{
+  current_test name_("timer testing");
   // the timer is intended to produce human-readable output
   // real testing is done by reading the output
   // check here that there are no crashes
@@ -26,54 +64,27 @@ TEST_CASE("test timer", "[timing test]")
   }
 
   double dur = tools::simple_timer::duration_since(start);
-  REQUIRE(dur >= 7.0); // must have waited above, keep this loose
+  tassert(dur >= 7.0); // must have waited above, keep this loose
 
   auto const ttime = tools::timer.stop("testing");
   ignore(ttime);
 #ifdef ASGARD_USE_TIMER
-  REQUIRE(ttime >= 7.0); // must have waited above, keep this loose
+  tassert(ttime >= 7.0); // must have waited above, keep this loose
 
   auto report = tools::timer.report();
-  REQUIRE(report.find("testing") < report.size());
-  REQUIRE(report.find("regulat session") < report.size());
-  REQUIRE(report.find("nested session") < report.size());
-  REQUIRE(report.find("100%") >= report.size());
+  tassert(report.find("testing") < report.size());
+  tassert(report.find("regulat session") < report.size());
+  tassert(report.find("nested session") < report.size());
+  tassert(report.find("100%") >= report.size());
 #endif
 }
 
-TEST_CASE("for-indexof testing", "[indexing testing]")
+int main(int, char**)
 {
-  std::vector<double> x(10);
-  std::vector<int64_t> r;
-  r.reserve(x.size());
+  all_tests global_("misc tools");
 
-  for (auto i : indexof(x))
-  {
-    static_assert(std::is_same_v<decltype(i), int64_t>);
-    r.push_back(i);
-  }
+  test_indexof();
+  test_timer();
 
-  REQUIRE(r.size() == x.size());
-  for (int64_t i = 0; i < 10; i++)
-    REQUIRE(r[i] == i);
-
-  std::vector<int> ir;
-  ir.reserve(8);
-
-  for (auto i : indexof<int>(1, 6))
-  {
-    static_assert(std::is_same_v<decltype(i), int>);
-    ir.push_back(i);
-  }
-
-  for (int i = 1; i < 6; i++)
-    REQUIRE(ir[i - 1] == i);
-
-  size_t s = 0;
-  for (auto i : indexof<size_t>(x.size()))
-  {
-    static_assert(std::is_same_v<decltype(i), size_t>);
-    s += i;
-  }
-  REQUIRE(s == 45);
+  return 0;
 }
