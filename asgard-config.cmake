c67a1d49271253c2a9d8ebdc091069a7f40da73b
Miroslav Stoyanov <30537612+mkstoyanov@users.noreply.github.com>
2025-09-29 12:55:57 -0400

    Cmake install (#805)
    * install fixes for cmake, using gnu install dirs

--- asgard-config.cmake
+++ asgard-config.cmake
@@ -6,7 +6,7 @@ endif()
 
 @PACKAGE_INIT@
 
-include("@__asgard_install_prefix@/lib/@CMAKE_PROJECT_NAME@/@CMAKE_PROJECT_NAME@-targets.cmake")
+include("@__asgard_install_prefix@/@CMAKE_INSTALL_LIBDIR@/cmake/@CMAKE_PROJECT_NAME@/@CMAKE_PROJECT_NAME@-targets.cmake")
 
 if ("@ASGARD_USE_MPI@" AND NOT TARGET MPI::MPI_CXX)
   if (NOT MPI_HOME AND NOT DEFINED ENV{MPI_HOME})
@@ -53,14 +53,19 @@ else()
 endif()
 
 if ("@ASGARD_USE_HIGHFIVE@")
-  enable_language (C)
-  if (@__asgard_find_hdf5@)
+  # if ASGarD build HDF5, then the C programming language is needed
+  # the targets will be loaded automatically since HDF5 is part of ASGarD exports
+  if ("@ASGARD_BUILD_HDF5@")
+    enable_language (C)
+  else()
+    # if using system HDF5, then just pull it through the regular channel
     find_package (HDF5 REQUIRED)
   endif()
 endif()
 
 add_executable(asgard::exe IMPORTED)
-set_property(TARGET asgard::exe PROPERTY IMPORTED_LOCATION "@__asgard_install_prefix@/bin/asgard${CMAKE_EXECUTABLE_SUFFIX_CXX}")
+set_property(TARGET asgard::exe PROPERTY IMPORTED_LOCATION
+             "@__asgard_install_prefix@/@CMAKE_INSTALL_BINDIR@/asgard${CMAKE_EXECUTABLE_SUFFIX_CXX}")
 
 add_library(asgard::asgard INTERFACE IMPORTED GLOBAL)
 target_link_libraries(asgard::asgard INTERFACE libasgard)
