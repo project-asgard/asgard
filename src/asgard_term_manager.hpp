2a6b7a081b6dfcb4c44a977e67d2f577c2d4f5c7
Miroslav Stoyanov <stoyanovmk@ornl.gov>
2025-11-18 15:39:30 -0500

    cleanup

--- src/asgard_term_manager.hpp
+++ src/asgard_term_manager.hpp
@@ -446,7 +446,6 @@ protected:
   void raw2cells(bool is_diag, int level, std::vector<P> &out);
   //! assign compute resources to the terms
   void assign_compute_resources();
-  void assign_compute_resources_v2();
 
 private:
   // workspace and workspace matrices
