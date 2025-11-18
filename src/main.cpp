198f9d2e942033fa337ec2f3db3e8e08272edf7b
Miroslav Stoyanov <stoyanovmk@ornl.gov>
2025-11-18 17:18:14 -0500

    better cmake logging

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,28 @@
 #include "asgard.hpp"
 
+#include "asgard_cmakelog.hpp"
+
 int main(int argc, char **argv)
 {
   asgard::prog_opts const options(argc, argv);
 
+  if (options.show_help) {
+    std::cout << "\n solves the continuity equation:\n";
+    std::cout << "    f_t + div f^2 = nu * f_xx + s(t, x)\n\n";
+    std::cout << "    -- standard ASGarD options --";
+    options.print_help(std::cout);
+    std::cout << "<< the asgard executable accepts one more option >>\n";
+    std::cout << "-cmake                              show a CMake build log\n\n";
+    return 0;
+  }
+
   // if there were unknown options, throw an error
-  options.throw_if_invalid();
+  options.throw_if_argv_not_in({"-cmake", }, {});
+
+  if (options.has_cli_entry("-cmake")) {
+    show_cmake_log();
+    return 0;
+  }
 
   if (options.show_help) {
     options.print_help();
