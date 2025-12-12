f4eda619cd7b35f42a0b60c0df324297110ca2c0
Miroslav Stoyanov <stoyanovmk@ornl.gov>
2025-12-11 21:04:28 -0500

    setting the pde scheme

--- src/pde/bgk.cpp
+++ src/pde/bgk.cpp
@@ -64,33 +64,33 @@ void self_test();
  * \snippet bgk.cpp asgard_examples_bgk make
  */
 template<typename P = asgard::default_precision>
-asgard::pde_scheme<P> make_bgk(int vdims, asgard::prog_opts options) {
+asgard::pde_scheme<P> make_bgk(int dims, asgard::prog_opts options) {
 #ifndef __ASGARD_DOXYGEN_SKIP
 //! [asgard_examples_bgk make]
 #endif
 
-  rassert(1 <= vdims and vdims <= 3, "problem is set for 1, 2 or 3 velocity dimensions")
+  rassert(1 <= dims and dims <= 3, "problem is set for 1, 2 or 3 velocity dimensions");
 
-  options.title = "Vlasov-Poisson-Lenard-Bernstein 1x" + std::to_string(vdims) + "v";
+  options.title = "Bhatnagar-Gross-Krook "
+                 + std::to_string(dims) + "x" + std::to_string(dims) + "v";
 
   // get the collision frequency
   P const nu = options.extra_cli_value_group<P>({"-nu", "-collision_freq"}).value_or(1.0);
   options.subtitle = "collision frequency: " + std::to_string(nu);
 
   std::vector<asgard::domain_range> ranges;
-  ranges.reserve(vdims + 1);
-  ranges.emplace_back(-2 * PI, 2 * PI);
-  for (int v = 0; v < vdims; v++)
+  ranges.reserve(2 * dims);
+  for (int x = 0; x < dims; x++)
+    ranges.emplace_back(-1.0, 1.0);
+  for (int v = 0; v < dims; v++)
     ranges.emplace_back(-6.0, 6.0);
 
   // the domain has one position and multiple velocity dimensions
-  asgard::pde_domain<P> domain(asgard::position_dims{1}, asgard::velocity_dims{vdims}, ranges);
+  asgard::pde_domain<P> domain(asgard::position_dims{dims}, asgard::velocity_dims{dims}, ranges);
 
   // setting some default options
   options.default_degree = 2;
-  options.default_start_levels = {6, 7};
-  for (int v = 1; v < vdims; v++)
-    options.default_start_levels.emplace_back(7);
+  options.default_start_levels = {4, };
 
   // using implicit-explicit stepper
   options.default_step_method = asgard::time_method::imex2;
@@ -107,35 +107,25 @@ asgard::pde_scheme<P> make_bgk(int vdims, asgard::prog_opts options) {
   options.default_isolver_iterations = 400;
   options.default_isolver_inner_iterations = 50;
 
-  options.default_precon = asgard::precon_method::jacobi;
+//   options.default_precon = asgard::precon_method::jacobi;
 
   // create a pde from the given options and domain
   asgard::pde_scheme<P> pde(options, domain);
 
-  // adding the terms for the pde
-  // the terms are split into two groups
-  // explicit Vlasov-Poisson, implicit Lenard-Bernstein
-  // each group corresponds to a set of terms that has been added constitutively
-  // 1. initialize a new term group, get the group-id
-  // 2. add the term from the group
-  // 3. move to the next group, or stop adding terms
-
   // adding the Vlasov-Poisson terms
   // the vp_group_id will persist until new_term_group() is called again
-  int const vp_group_id = pde.new_term_group();
+  int const v_group_id = pde.new_term_group();
 
   // see the two-stream instability example for details
   auto positive = [](std::vector<P> const &x, std::vector<P> &y)
-      -> void
-    {
+      -> void {
 #pragma omp parallel for
       for (size_t i = 0; i < x.size(); i++)
         y[i] = std::max(P{0}, x[i]);
     };
 
   auto negative = [](std::vector<P> const &x, std::vector<P> &y)
-      -> void
-    {
+      -> void {
 #pragma omp parallel for
       for (size_t i = 0; i < x.size(); i++)
         y[i] = std::min(P{0}, x[i]);
@@ -151,86 +141,69 @@ asgard::pde_scheme<P> make_bgk(int vdims, asgard::prog_opts options) {
       asgard::term_volume<P>(negative),
     };
 
-  std::vector<asgard::term_1d<P>> dv_Epositive = {
-      asgard::volume_electric<P>(positive),
-      asgard::term_div<P>(1, asgard::flux_type::upwind, asgard::boundary_type::bothsides)
-    };
+  int const b_group_id = pde.new_term_group();
 
-  std::vector<asgard::term_1d<P>> dv_Enegative = {
-      asgard::volume_electric<P>(negative),
-      asgard::term_div<P>(1, asgard::flux_type::downwind, asgard::boundary_type::bothsides)
-    };
+  pde += asgard::term_md<P>({asgard::term_volume<P>{nu}, asgard::term_identity{}});
 
-  // pad with identity term for dimensions after v1
-  for (int v = 1; v < vdims; v++) {
-    dx_positive.emplace_back(asgard::term_identity{});
-    dx_negative.emplace_back(asgard::term_identity{});
-    dv_Epositive.emplace_back(asgard::term_identity{});
-    dv_Enegative.emplace_back(asgard::term_identity{});
-  }
+  asgard::moment_id im0 = pde.register_moment(asgard::moment(0));
+  asgard::moment_id im1 = pde.register_moment(asgard::moment(1));
+  asgard::moment_id im2 = pde.register_moment(asgard::moment(2));
 
-  pde += dx_positive;
-  pde += dx_negative;
-  pde += dv_Epositive;
-  pde += dv_Enegative;
+  auto fbgk = [=](P /* time */, asgard::vector2d<P> const &nodes,
+                  asgard::momentset<P> const &moments, std::vector<P> &vals)
+  {
+    std::vector<P> const &m0 = moments[im0];
+    std::vector<P> const &m1 = moments[im1];
+    std::vector<P> const &m2 = moments[im2];
 
-  // here, the Vlasov-Poisson group will be finalized
-  // moving over to the lenard-bernstein group
-  int const lb_group_id = pde.new_term_group();
+    int64_t const num_nodes = nodes.num_strips();
+#pragma omp parallel for
+    for (int64_t i = 0; i < num_nodes; i++) {
+      // P const x = nodes[i][0];
+      P const v = nodes[i][1];
 
-  pde += asgard::operators::lenard_bernstein_collisions{nu};
+      P const n = m0[i];
+      P const u = m1[i] / m0[i];
+      P const t = m2[i] / m0[i] - u * u;
 
-  // adding penalty
-  double const pen = 10.0 / pde.min_cell_size(1);
-  std::vector<asgard::term_1d<P>> penop = {
-      asgard::term_identity{},
-      asgard::term_penalty<P>(pen, asgard::flux_type::upwind, asgard::boundary_type::none)
-    };
+      vals[i] = nu * n / std::sqrt(2 * PI * t);
+      P const d = v - u;
+      vals[i] *= std::exp(- P{0.5} * d * d / t);
+    }
+  };
 
-  for (int v = 1; v < vdims; v++)
-    penop.emplace_back(asgard::term_identity{});
+  auto abgk = [=](P time, asgard::vector2d<P> const &nodes,
+                  asgard::momentset<P> const &moments, std::vector<P> const &,
+                  std::vector<P> &vals)
+  {
+    fbgk(time, nodes, moments, vals);
+  };
 
-  pde += penop;
+  pde.set_adapt_weight({im0, im1, im2}, abgk);
 
-  // finished with the terms
-
-  // using IMEX stepper requires that the pde "knows" the corresponding groups
-  // the technique used here is called "strong-types" for C++,
-  // e.g., see here: https://www.fluentcpp.com/2016/12/08/strong-types-for-strong-interfaces/
+  pde.set_source(asgard::moment_source<P>(fbgk, {im0, im1, im2}));
 
   // set the implicit and explicit operator groups
-  pde.set(asgard::imex_implicit_group{lb_group_id},
-          asgard::imex_explicit_group{vp_group_id});
+  pde.set(asgard::imex_implicit_group{b_group_id},
+          asgard::imex_explicit_group{v_group_id});
+
 
   // separable initial conditions in x and v
   auto ic_x = [](std::vector<P> const &x, P /* time */, std::vector<P> &fx) ->
     void {
       for (size_t i = 0; i < x.size(); i++)
-        fx[i] = 1.0 + 1.E-4 * std::cos(0.5 * x[i]);
+        fx[i] = 1;
     };
 
   auto ic_v = [](std::vector<P> const &v, P /* time */, std::vector<P> &fv) ->
     void {
-      P const c = P{1} / std::sqrt(2 * PI);
+      // P const c = P{1} / std::sqrt(2 * PI);
 
       for (size_t i = 0; i < v.size(); i++)
-        fv[i] = c * std::exp(-0.5 * v[i] * v[i]);
+        fv[i] = 1;
     };
 
-  // setting the initial conditions based on the number of dimensions
-  switch (vdims) {
-    case 1:
-      pde.add_initial(asgard::separable_func<P>({ic_x, ic_v}));
-      break;
-    case 2:
-      pde.add_initial(asgard::separable_func<P>({ic_x, ic_v, ic_v}));
-      break;
-    case 3:
-      pde.add_initial(asgard::separable_func<P>({ic_x, ic_v, ic_v, ic_v}));
-      break;
-    default:
-      break;
-  }
+  pde.add_initial(asgard::separable_func<P>({ic_x, ic_v}));
 
   return pde;
 
@@ -263,28 +236,29 @@ std::vector<P> compute_perturbation(asgard::discretization_manager<P> const &dis
 //! [asgard_examples_bgk compute_perturbation]
 #endif
 
-  // The Maxwellian is the initial condition
-  // but with constant value of 1.0 set in dimension 0 (the position dimension)
-  asgard::separable_func<P> maxw = disc.initial_cond_sep().front();
-
-  // set dimension 0 to be a constant function with value 1
-  maxw.set(0, P{1});
-
-  // project the Maxwellian onto the current grid
-  std::vector<P> proj_max = disc.project_function(maxw);
-
-  // subtract the current state
-  std::vector<P> const &state = disc.current_state();
-
-  // the projected size will always match the size of the current state
-  if (proj_max.size() != state.size())
-    throw std::runtime_error("this will never happen");
-
-  size_t n = state.size();
-  for (size_t i = 0; i < n; i++)
-    proj_max[i] -= state[i];
-
-  return proj_max;
+  // // The Maxwellian is the initial condition
+  // // but with constant value of 1.0 set in dimension 0 (the position dimension)
+  // asgard::separable_func<P> maxw = disc.initial_cond_sep().front();
+  //
+  // // set dimension 0 to be a constant function with value 1
+  // maxw.set(0, P{1});
+  //
+  // // project the Maxwellian onto the current grid
+  // std::vector<P> proj_max = disc.project_function(maxw);
+  //
+  // // subtract the current state
+  // std::vector<P> const &state = disc.current_state();
+  //
+  // // the projected size will always match the size of the current state
+  // if (proj_max.size() != state.size())
+  //   throw std::runtime_error("this will never happen");
+  //
+  // size_t n = state.size();
+  // for (size_t i = 0; i < n; i++)
+  //   proj_max[i] -= state[i];
+  //
+  // return proj_max;
+  return std::vector<P>(disc.state_size());
 
 #ifndef __ASGARD_DOXYGEN_SKIP
 //! [asgard_examples_bgk compute_perturbation]
@@ -388,41 +362,41 @@ void test_energy(int const vdims, std::string const &opt_str) {
 
   // the pde needs only the zeroth moment and computes that internally
   // we are using the other moments to check energy conservation properties
-  auto pde = make_bgk<P>(vdims, options);
-  moment_id const m2 = pde.register_moment({2, moment::inactive});
-  discretization_manager disc(std::move(pde), verbosity_level::quiet);
-
-  double E0 = 0; // initial total energy (potential + kinetic), will initialize on first iteration
-
-  int64_t const n = disc.remaining_steps();
-
-  P constexpr tol = (std::is_same_v<P, double>) ? 5.E-7 : 5.E-3;
-
-  for (int64_t i = 0; i < n; i++)
-  {
-    disc.advance_time(1);
-
-    int const level0   = disc.get_grid().current_level(0);
-    int const num_cell = fm::ipow2(level0);
-    P const dx         = disc.domain().length(0) / num_cell;
-
-    auto efield = disc.get_electric();
-
-    double Ep = 0;
-    for (auto e : efield)
-      Ep += e * e;
-    Ep *= dx;
-
-    std::vector<P> mom2 = disc.get_moment(m2);
-
-    P const Ek = mom2[0] * std::sqrt(disc.domain().length(0));
-
-    if (disc.current_step() == 1) // first time-step
-      E0 = Ep + Ek;
-
-    // check the initial slight energy decay before it stabilizes
-    tcheckless(i, std::abs(Ep + Ek - E0), tol);
-  }
+  // auto pde = make_bgk<P>(vdims, options);
+  // moment_id const m2 = pde.register_moment({2, moment::inactive});
+  // discretization_manager disc(std::move(pde), verbosity_level::quiet);
+  //
+  // double E0 = 0; // initial total energy (potential + kinetic), will initialize on first iteration
+  //
+  // int64_t const n = disc.remaining_steps();
+  //
+  // P constexpr tol = (std::is_same_v<P, double>) ? 5.E-7 : 5.E-3;
+  //
+  // for (int64_t i = 0; i < n; i++)
+  // {
+  //   disc.advance_time(1);
+  //
+  //   int const level0   = disc.get_grid().current_level(0);
+  //   int const num_cell = fm::ipow2(level0);
+  //   P const dx         = disc.domain().length(0) / num_cell;
+  //
+  //   auto efield = disc.get_electric();
+  //
+  //   double Ep = 0;
+  //   for (auto e : efield)
+  //     Ep += e * e;
+  //   Ep *= dx;
+  //
+  //   std::vector<P> mom2 = disc.get_moment(m2);
+  //
+  //   P const Ek = mom2[0] * std::sqrt(disc.domain().length(0));
+  //
+  //   if (disc.current_step() == 1) // first time-step
+  //     E0 = Ep + Ek;
+  //
+  //   // check the initial slight energy decay before it stabilizes
+  //   tcheckless(i, std::abs(Ep + Ek - E0), tol);
+  // }
 }
 
 void self_test() {
