fc7b9452666e60ace0011371a0fb3bbf51cbb4dc
Miroslav Stoyanov <stoyanovmk@ornl.gov>
2026-03-24 12:37:21 -0400

    fix ignore

--- src/asgard_discretization.cpp
+++ src/asgard_discretization.cpp
@@ -316,7 +316,7 @@ void discretization_manager<precision>::save_snapshot(std::filesystem::path cons
   h5manager<precision>::write(options_, domain_, degree(), grid, stepper.data,
                               state, terms.moms, aux_fields, filename);
 #else
-  ignore(filename);
+  std::ignore = filename;
   throw std::runtime_error("saving to a file requires CMake option -DASGARD_USE_HIGHFIVE=ON");
 #endif
 }
